ad54b8fe3955a759b88a3ad6d25f3a300a532470
Pavel Bushlanov <paulbush@mail.ru>
2018-04-12 13:38:20 +0300

    make loop class field

--- USPEX/Common/Worker.py
+++ USPEX/Common/Worker.py
@@ -13,13 +13,14 @@ class Worker(object):
 
     shortname = None
     workers = {}
+    loop = None
 
-    def __init__(self, loop, name):
+    def __init__(self, name):
         super().__init__()
-        self.loop = loop
         self.name = name
 
-    def sendTask(self, *args, **kwargs):
+    @classmethod
+    def sendTask(cls, *args, **kwargs):
         data = list(args)
         if kwargs:
             data.append(kwargs)
@@ -29,38 +30,26 @@ class Worker(object):
                 if 'name' in dct:
                     name = dct['name']
                     if 'kill' in dct:
-                        del self.workers[name]
-                    elif name not in self.workers:
-                        self.workers[name] = getSubClassByName(Worker, dct['type'])(self.loop, name, **dct['params'])
+                        del cls.workers[name]
+                    elif name not in cls.workers:
+                        cls.workers[name] = getSubClassByName(Worker, dct['type'])(name, **dct['params'])
             except KeyError as e:
                 print('Warning: KeyError: {}, data : {}'.format(e, data))
                 continue
 
         for dct in data:
             try:
-                if 'toWorker' in dct and dct['toWorker'] in self.workers:
-                    asyncio.ensure_future(self.workers[dct['toWorker']].run(dct['fromWorker'],**dct['data']))
+                if 'toWorker' in dct and dct['toWorker'] in cls.workers:
+                    asyncio.ensure_future(cls.workers[dct['toWorker']].run(dct['fromWorker'],**dct['data']))
             except KeyError as e:
                 print('Warning: KeyError: {}, data : {}'.format(e, data))
                 continue
 
-        if not self.workers:
-            self.loop.stop()
+        if not cls.workers:
+            cls.loop.stop()
 
     async def run(self, fromWorker : str):
         pass
-
-    ############################################
-    # Code responds for serialization
-    def __getstate__(self):
-        state = copy(self.__dict__)
-        del state['loop']
-        return state
-
-    def __setstate__(self, state):
-        self.__dict__ = state
-        self.loop = asyncio.get_event_loop()
-
 
     @classmethod
     def save(cls):
@@ -68,8 +57,6 @@ class Worker(object):
             pcl.dump(cls.workers, f)
 
     @classmethod
-    def load(cls, loop):
+    def load(cls):
         with open('workers.dump', 'rb') as f:
             cls.workers = pcl.load(f)
-        for worker in cls.workers.values():
-            worker.loop = loop
