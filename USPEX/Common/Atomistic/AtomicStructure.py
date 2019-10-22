7cc71cc49ed1869314ed190aa77160a7af063397
Pavel Bushlanov <paulbush@mail.ru>
2019-10-22 19:10:40 +0300

    remove notes from AtomicStructure

--- USPEX/Common/Atomistic/AtomicStructure.py
+++ USPEX/Common/Atomistic/AtomicStructure.py
@@ -40,7 +40,6 @@ class AtomicStructure(object):
     forces = None
     _pressureTensor = None
     dimension = None
-    notes = {}
 
     def __init__(self, molecules=[], symbols=None, positions=None, scaled_positions=None, cell=None, pbc=None, info=None,
                  optimizeLattice=False, magmoms=None):
