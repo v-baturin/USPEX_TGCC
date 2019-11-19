f4dea09dfe6cf6db098477531df25c06b0666c07
Pavel Bushlanov <paulbush@mail.ru>
2019-11-19 14:19:49 +0300

    add tests for AtomicStucture isMoleculesDistinct

---
+++ USPEX/Common/Atomistic/unittests/AtomicStructure_Test.py
@@ -0,0 +1,25 @@
+'''
+@file        ConvexHullTest.py
+@author:     Pavel Bushlanov
+@copyright:  2019 Oganov's Lab. All rights reserved.
+@contact:    paulbush@mail.ru
+@date        17 November 2019
+@brief       Class for AtomicStructure testing
+'''
+
+import unittest
+
+from ..Crystal import Crystal
+
+
+class AtomicStructure_Test(unittest.TestCase):
+
+    def test_isMoleculesDistinct1(self):
+        with open("CNHO_1_system", "rt") as f:
+            system = Crystal.fromJSON(f.read())
+        self.assertFalse(system.isMoleculesDistinct())
+
+    def test_isMoleculesDistinct2(self):
+        with open("CNHO_2_system", "rt") as f:
+            system = Crystal.fromJSON(f.read())
+        self.assertTrue(system.isMoleculesDistinct())
