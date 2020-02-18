Updated logic inside Bond and Bonds classes/ Now it it much better to work with it. Also fixed using onle MaxBonds for softmodes calculation

USPEX/Common/Atomistic/AtomisticConfig.py
@@ -11,9 +11,10 @@
 import logging
 import numpy as np
 
+from ase.geometry import get_distances
 from copy import copy
 from itertools import combinations_with_replacement, chain
-from ase.geometry import get_distances
+from typing import Dict, List
 
 
 from ..Config import Config
@@ -57,8 +58,8 @@ class ChemicalConfig(Config):
     CenterminDistMatrice = None
 
     def __init__(self, symbols : list, volumeType : str=None, ionDistances : dict=None, goodBonds : list=None,
-                 valences : list=None, minVectorLength : int=None, valenceElectrons : list=None,
-                 externalPressure : float=0.0001, moleculesDistinctCheck = True, MolCenters : dict=None, **kwargs):
+                       valences : Dict[str, float]=None, minVectorLength : dict=None, valenceElectrons : Dict[str, float]=None,
+                       externalPressure : float=0.0001, moleculesDistinctCheck = True, MolCenters : dict=None, **kwargs):
         '''
 
         :param symbols:
@@ -104,15 +105,8 @@ class ChemicalConfig(Config):
         else:
             self.goodBonds = defaultGoodBonds(self.chemicalSymbols)
 
-        if valences is not None:
-            self.valences = np.asarray(valences)
-        else:
-            self.valences = np.asarray([Element(symbol).valence for symbol in self.chemicalSymbols])
-
-        if valenceElectrons is not None:
-            self.valenceElectrons = np.asarray(valenceElectrons)
-        else:
-            self.valenceElectrons = np.asarray([Element(symbol).valence_electrons for symbol in self.chemicalSymbols], dtype=int)
+        self.valences = valences if valences is not None else {symbol: Element(symbol).valence for symbol in self.chemicalSymbols}
+        self.valenceElectrons = valenceElectrons if valenceElectrons is not None else {symbol: Element(symbol).valence_electrons for symbol in self.chemicalSymbols}
 
         self._minVectorLength = minVectorLength if isinstance(minVectorLength, float) and minVectorLength > 0.0 else None
 

USPEX/Common/Atomistic/Bonds.py
@@ -1,4 +1,10 @@
 import numpy as np
+
+from ase.atom import Atom
+from ase.neighborlist import primitive_neighbor_list
+from dataclasses import dataclass
+from typing import List, Union, Tuple
+
 from USPEX.Common.Atomistic.Element import Element
 
 
@@ -22,9 +28,21 @@ def dfs1(graph : dict, root : int):
     return visited
 
 
+# @dataclass
+# class B_Atom:
+#     name : str
+#     index : int
+#
+#     def __init__(self, name:str, index:int):
+#         assert index >= 0
+#         self.index = index
+#         self.name = name
+
+
+@dataclass
 class Bond(object):
     '''
-    Class for the chemical bond description. It has follow parametes:
+    Class for the chemical bond description. It has follow parameters:
 
     * _atom1 (int): ID of the fisrt atom in bond
     * _atom2 (int): ID of the second atom in bond
@@ -38,21 +56,19 @@ class Bond(object):
     * LOWER_BOUND = 0.5           # (Angstrems) lower bound for the distance of atoms in a bond.
     '''
 
-    _atom1 = None
-    _atom2 = None
+    _atom1 : Atom
+    _atom2 : Atom
+    distance : float
+    delta : float
+    type : Union[int, str, None]
+    enable : bool = False
 
-    type = None
-    delta = 0.0
-    enable = False
+    SAME_BOND_THRESHOLD : float = 0.05  # (Angstrems) same bond within this distance.
+    MAX_BOND : float = 5.0              # (Angstrems) maximum distance deviation for bonds search.
+    LOWER_BOUND : float = 0.5           # (Angstrems) lower bound for the distance of atoms in a bond.
 
-    # MAX_BOND: maximum distance deviation for bonds search.
-    # SAME_BOND_THRESHOLD: same bond within this distance between same type of atoms.
-
-    SAME_BOND_THRESHOLD = 0.05  # (Angstrems) same bond within this distance.
-    MAX_BOND = 5.0              # (Angstrems) maximum distance deviation for bonds search.
-    LOWER_BOUND = 0.5           # (Angstrems) lower bound for the distance of atoms in a bond.
-
-    def __init__(self, atom1 : int, atom2 : int, distance : float, type=None, direction=[0,0,0]):
+    def __init__(self, atom1 : Atom, atom2 : Atom, distance : float, type=None,
+                 direction : List[int] = [0,0,0], vector : List[float] = None):
         '''
         :param atom1: 
         :param atom2: 
@@ -60,14 +76,27 @@ class Bond(object):
         :param type: 
         :param direction: 
         '''
-        assert atom1 >= 0 and atom2 >= 0
         assert 3 == len(direction)
-        # assert distance > 0.0 and distance < self.MAX_BOND
+        assert distance > 0.0
+
+        R_val = lambda symbol: Element(symbol).covalent_radius
+
         self._atom1, self._atom2 = atom1, atom2
         self.type = type
-        self.delta = distance
+        self.distance = distance
+        self.delta = distance - R_val(atom1.symbol) - R_val(atom2.symbol)
         self.direction = direction
+        self.vector = vector
 
+    @property
+    def indicies(self) -> Tuple[int, int]:
+        return self._atom1.index, self._atom2.index
+
+    @property
+    def symbols(self) -> Tuple[str, str]:
+        return self._atom1.symbol, self._atom2.symbol
+
+    @property
     def atoms(self):
         '''
         :return: indices of origin and end atoms in bond.
@@ -75,15 +104,17 @@ class Bond(object):
         return self._atom1, self._atom2
 
     def __eq__(self, other) -> bool:
-        assert isinstance(other, Bond)
-        return self.delta - other.delta < self.SAME_BOND_THRESHOLD
+        isEqual_Symbols = self.symbols == other.symbols or self.symbols == reversed(other.symbols)
+        isEqualDistance = np.abs(self.distance - other.distance) < self.SAME_BOND_THRESHOLD
+        return isEqual_Symbols and isEqualDistance
 
     def __ne__(self, other) -> bool:
-        assert isinstance(other, Bond)
-        return self.delta - other.delta > self.SAME_BOND_THRESHOLD
+        isEqual_Symbols = self.symbols == other.symbols or self.symbols == reversed(other.symbols)
+        isEqualDistance = np.abs(self.distance - other.distance) < self.SAME_BOND_THRESHOLD
+        return not (isEqual_Symbols and isEqualDistance)
 
 
-class Bonds(list):
+class Bonds(List[Bond]):
     '''
     Class for the list of the bonds.
     '''
@@ -105,12 +136,13 @@ class Bonds(list):
         return set([x.type for x in self])
 
     # TODO do it
-    def is3Dconnected(self) -> bool:
+    def _is3Dconnected(self) -> bool:
         X = np.max([np.abs(bond.direction[0]) for bond in self])
         Y = np.max([np.abs(bond.direction[1]) for bond in self])
         Z = np.max([np.abs(bond.direction[2]) for bond in self])
         return X > 0 and Y > 0 and Z > 0
 
+    # TODO fix it
     def connectList(self):
         '''
         IMPORTANT NOTE: this implementation checks connectivity of the atoms INSIDE unit cell.
@@ -144,6 +176,7 @@ class Bonds(list):
 
     def __add__(self, other):
         return Bonds(super(Bonds,self).__add__(other))
+
 
 def defaultGoodBonds(symbols):
     '''

USPEX/Common/Atomistic/softmodes/Softmodes.py
@@ -46,26 +46,16 @@ class Softmodes(list):
         :param kvector:
         '''
 
+        assert hasattr(config, 'valences')
+        assert hasattr(config, 'valenceElectrons')
+        assert hasattr(config, 'goodBonds')
+
         super(Softmodes, self).__init__()
         # Inputs:
         self._config = config
         self.system = system
 
-        # Calculate "smart" default values:
-
-        atomTypes, atom_type_seq = atomTypeCounter(system.chemicalSymbols)
-
-        if hasattr(config, 'valences'):
-            self.val = config.valences
-        else:
-            self.val = [Element(x).valence for x in atomTypes]
-
-        if hasattr(config, 'valenceElectrons'):
-            self.N_val = config.valenceElectrons
-        else:
-            self.N_val = [Element(x).valence_electrons for x in atomTypes]
-        self.R_val = np.array([Element(atomType).covalent_radius for atomType in atomTypes])
-
+        self.R_val = {symbol : Element(symbol).covalent_radius for symbol in set(system.chemicalSymbols)}
         system.bonds = BondHardness_new(system, config.goodBonds)
         self._calc_soft_modes(system, kvector)
 
@@ -81,7 +71,7 @@ class Softmodes(list):
     def _calc_soft_modes(self, system, kvector=np.zeros(3)):
         start_time = time.time()
         # each eigenvector has written as column. So, next we are using transpose matrix.
-        frequencies, eigenvectors = calcSoftModes(system, self.R_val, self.N_val, self.val, kvector)
+        frequencies, eigenvectors = calcSoftModes(system, self.R_val, self._config.valenceElectrons, self._config.valences, kvector)
         self.extend([VibrationalMode(f, v) for f, v in zip(frequencies, eigenvectors.T)])
         self.duration_sm = time.time() - start_time
 

USPEX/Common/Atomistic/softmodes_new/MaxBonds.py
@@ -17,19 +17,40 @@ from .AtomTypeCounter import atomTypeCounter
 def MaxBonds_new(system, cutoff:float=Bond.MAX_BOND) -> Bonds:
 
     symbols = system.chemicalSymbols
-    R_val = lambda symbol: Element(symbol).covalent_radius
+
+    bonds = Bonds()
+    i_init, j_init, dists, vecs, dirs = primitive_neighbor_list(quantities='ijdDS', pbc=system.pbc,
+                                                          cell=system.get_cell(complete=True),
+                                                          positions=system.get_scaled_positions(),
+                                                          cutoff=cutoff, numbers=system.numbers,
+                                                          use_scaled_positions=True)
+
+    for i, j, dist, vec, dir in zip(i_init, j_init, dists, vecs, dirs):
+        # TODO Why we had this less 0.5A and not more than 5A (usually)
+        # if np.abs(dist - tmp_Rval) > cutoff or dist < 0.5:
+        if dist < 0.5:
+            continue
+        bonds.append(Bond(atom1=system[i], atom2=system[j], direction=dir, distance=dist, vector=vec))
 
 
-    res_bonds = Bonds()
-    i_init, j_init, dist_init, dir_init = primitive_neighbor_list(quantities='ijdS', pbc=system.pbc,
-                                                        cell=system.get_cell(complete=True),
-                                                        positions=system.get_scaled_positions(),
-                                                        cutoff=cutoff, numbers=system.numbers,
-                                                        use_scaled_positions=True)
+    # bonds: (1) atom-i, (2) atom-j, (3) dist, (4) bond_type.
+    # Now we assign the bond type
 
-    for i,j,dist, dir in zip(i_init, j_init, dist_init, dir_init):
-        res_bonds.append(Bond(atom1=i, atom2=j, direction=dir, distance=dist-(R_val(symbols[i]) + R_val(symbols[j]))))
-    return res_bonds
+    # rank = np.argsort([b.distance for b in tmp_bonds])  # sort the bonds by dist from low to high
+    # tmp_bonds = tmp_bonds[rank]
+    tmp_bonds = sorted(bonds, key=lambda x: x.distance)
+
+    # N_bonds = bonds.shape[0]
+    bond_type:int = 0
+    for bond in tmp_bonds:
+        if not bond.type:
+            bond_type += 1
+            # symbols_ref = sorted(bond.symbols)
+            # Obtain all bonds with the same type by distance:
+            for b in [b for b in tmp_bonds if b == bond]:
+                if not b.type:
+                    b.type = bond_type
+    return bonds
 
 
 def MaxBonds(SYSTEM : AtomicStructure) -> Bonds:

USPEX/Common/Atomistic/softmodes_new/Softmodes.py
@@ -7,12 +7,15 @@ import numpy as np
 from USPEX.Common.Config import Config
 from USPEX.Common.Atomistic.Element import Element
 from USPEX.Common.Atomistic.AtomicStructure import AtomicStructure
+
+from USPEX.Common.Atomistic.Bonds import Bonds
 from .BondHardness_new import BondHardness_new
-from .MaxBonds import MaxBonds_new
+from .MaxBonds import MaxBonds, MaxBonds_new
 from .calcHardness import calcHardness
 from .calcSoftModes import calcSoftModes
 from .AtomTypeCounter import atomTypeCounter
 
+from typing import List
 
 warnings.filterwarnings('ignore')
 
@@ -21,10 +24,10 @@ class VibrationalMode(object):
     frequency = None
     eigenvector = None
 
-    def __init__(self, FREQUENCY, EIGENVECTOR, SUPERCELL=np.ones(3)):
-        self.frequency = float(FREQUENCY)
-        self.eigenvector = np.array(EIGENVECTOR)
-        self.supercell = np.array(SUPERCELL)
+    def __init__(self, frequency : float, eigenvector : List[float], supercell : List[int]=np.ones(3)):
+        self.frequency = frequency
+        self.eigenvector = eigenvector
+        self.supercell = supercell
 
 
 class Softmodes(list):
@@ -34,12 +37,9 @@ class Softmodes(list):
     :param kvector: k-vector specified by user.
     '''
 
-    modes = []
+    duration_h = -np.inf
 
-    duration_h = 0.0
-    duration_sm = 0.0
-
-    def __init__(self, config : Config, system : AtomicStructure, kvector=np.zeros(3)):
+    def __init__(self, config : Config, system : AtomicStructure, kvector : List[int]=np.zeros(3)):
         '''
 
         :param config:
@@ -56,21 +56,25 @@ class Softmodes(list):
 
         atomTypes, atom_type_seq = atomTypeCounter(system.chemicalSymbols)
 
-        if hasattr(config, 'valences'):
-            self.val = config.valences
-        else:
-            self.val = [Element(x).valence for x in atomTypes]
-
-        if hasattr(config, 'valenceElectrons'):
-            self.N_val = config.valenceElectrons
-        else:
-            self.N_val = [Element(x).valence_electrons for x in atomTypes]
-        self.R_val = np.array([Element(atomType).covalent_radius for atomType in atomTypes])
+        # if hasattr(config, 'valences'):
+        #     self.val = config.valences
+        # else:
+        #     self.val = [Element(x).valence for x in atomTypes]
+        #
+        # if hasattr(config, 'valenceElectrons'):
+        #     self.N_val = config.valenceElectrons
+        # else:
+        #     self.N_val = [Element(x).valence_electrons for x in atomTypes]
+        # self.R_val = np.array([Element(atomType).covalent_radius for atomType in atomTypes])
 
         start_time = time.time()
-        system.bonds = MaxBonds_new(system)
+        # bonds = MaxBonds(system)
         self.duration_bh = time.time() - start_time
-        self._calc_soft_modes(system, kvector)
+        start_time = time.time()
+        bonds_new = MaxBonds_new(system)
+        self.duration_bh_new = time.time() - start_time
+        # self._calc_soft_modes(system, bonds, kvector)
+        self._calc_soft_modes(system, bonds_new, kvector)
 
     def hardness(self):
         '''
@@ -81,10 +85,11 @@ class Softmodes(list):
         self.duration_h = time.time() - start_time
         return hardness
 
-    def _calc_soft_modes(self, system, kvector=np.zeros(3)):
+    def _calc_soft_modes(self, system, bonds:Bonds, kvector : List[int]=np.zeros(3)):
         start_time = time.time()
         # each eigenvector has written as column. So, next we are using transpose matrix.
-        frequencies, eigenvectors = calcSoftModes(system, self.R_val, self.N_val, self.val, kvector)
+        # self.R_val, self.N_val, self.val,
+        frequencies, eigenvectors = calcSoftModes(system, self._config, bonds, kvector)
         self.extend([VibrationalMode(f, v) for f, v in zip(frequencies, eigenvectors.T)])
         self.duration_sm = time.time() - start_time
 

USPEX/Common/Atomistic/softmodes_new/calcSoftModes.py
@@ -1,11 +1,17 @@
 from __future__ import division
 
 from USPEX.Common.Atomistic.AtomicStructure import AtomicStructure
-from ..Bonds import Bonds
-from .AtomTypeCounter import atomTypeCounter
+from USPEX.Common.Atomistic.Bonds import Bonds
+
+from USPEX.Common.Atomistic.Element import Element
+
+# from .AtomTypeCounter import atomTypeCounter
 
 
 import numpy as np
+
+
+R_val = lambda symbol: Element(symbol).covalent_radius
 
 
 def AddDynMatSelf(D, a, H, Cos, phase):
@@ -35,7 +41,8 @@ def AddDynMat(D, a, b, H, Cos, phase1, phase2):
     return D
 
 
-def calcSoftModes(system : AtomicStructure, R_val, N_val, val, kVector0=np.zeros(3)):
+# R_val : dict, N_val : dict, val : dict,
+def calcSoftModes(system : AtomicStructure, config, bonds, kVector0=np.zeros(3)):
     '''
     The function calculates vibrational modes based on the dynamic matrix (D) constructed from bond hardness model.
 
@@ -52,7 +59,7 @@ def calcSoftModes(system : AtomicStructure, R_val, N_val, val, kVector0=np.zeros
     :return eigvector: eigenvector of all modes.
     '''
 
-    assert isinstance(system.bonds, Bonds)
+    # assert isinstance(system.bonds, Bonds)
 
     # Convert everything to ndarray:
     lat = system.cell
@@ -66,7 +73,7 @@ def calcSoftModes(system : AtomicStructure, R_val, N_val, val, kVector0=np.zeros
     kVector = np.dot(kVector0, rec_lat)
 
     # Obtain the bond information:
-    bond_groups = system.bonds.all_types()    # list of bond group
+    bond_groups = bonds.all_types()    # list of bond group
     N_group = len(bond_groups)           # number of bond group
     N_atom = len(system)  # number of atoms
 
@@ -77,50 +84,46 @@ def calcSoftModes(system : AtomicStructure, R_val, N_val, val, kVector0=np.zeros
     # nu_factor should be normalized to satisfy sum rule.
     nu_factor = np.zeros(N_atom)
 
-    atomTypes, atom_type_seq = atomTypeCounter(system.chemicalSymbols)
-    for k in range(N_atom):
+    # atomTypes, atom_type_seq = atomTypeCounter(system.chemicalSymbols)
+    for k, s in zip(range(N_atom), system.get_chemical_symbols()):
         nu_full = 0.0
-        for bond in system.bonds:
-            a, b = bond.atoms()
+        for bond in bonds:
+            a, b = bond.indicies
             if a == k:
                 nu_full += np.exp(-bond.delta / 0.37)
             if b == k:
                 nu_full += np.exp(-bond.delta / 0.37)
-        nu_factor[k] = val[atom_type_seq[k]] / nu_full
+        nu_factor[k] = config.valences[s] / nu_full
 
     for bondtype in bond_groups:
-        bonds = system.bonds.getType(bondtype)
-
-        for bond in bonds:
-            ID1, ID2 = bond.atoms()
-            a = atom_type_seq[ID1]
-            b = atom_type_seq[ID2]
-            R = R_val[a] + R_val[b] + bond.delta
-            R_val_sum = R_val[a] + R_val[b]
-            R_a = R_val[a]/R_val_sum * R
-            R_b = R_val[b]/R_val_sum * R
+        for bond in bonds.getType(bondtype):
+            s1, s2 = bond.symbols
+            i1, i2 = bond.indicies
+            # a = atom_type_seq[ID1]
+            # b = atom_type_seq[ID2]
+            R = R_val(s1) + R_val(s2) + bond.delta
+            R_val_sum = R_val(s1) + R_val(s2)
+            R_a = R_val(s1)/R_val_sum * R
+            R_b = R_val(s2)/R_val_sum * R
             nu = np.exp(-bond.delta / 0.37)
-            EN_a = 0.481 * N_val[a] / R_a
-            EN_b = 0.481 * N_val[b] / R_b
-            CN_a = val[a] / (nu * nu_factor[ID1])
-            CN_b = val[b] / (nu * nu_factor[ID2])
+            EN_a = 0.481 * config.valenceElectrons[s1] / R_a
+            EN_b = 0.481 * config.valenceElectrons[s2] / R_b
+            CN_a = config.valences[s1] / (nu * nu_factor[i1])
+            CN_b = config.valences[s2] / (nu * nu_factor[i2])
 
             f_ab = 0.25 * np.abs(EN_a - EN_b) / np.sqrt(EN_a * EN_b)
             X_ab = np.sqrt(EN_a * EN_b / (CN_a * CN_b))
 
-            vect = coords[ID1, :] - coords[ID2, :] - bond.direction
-            dR_ab = np.dot(vect, lat)
-
-            C = np.round(dR_ab / np.linalg.norm(dR_ab) * 1000000) / 1000000
-            phase_k1 = np.real(np.exp(1j * np.dot(kVector, dR_ab)))
-            phase_k2 = np.real(np.exp(1j * np.dot(kVector, -dR_ab)))
+            C = np.round(bond.vector / np.linalg.norm(bond.vector) * 1000000) / 1000000
+            phase_k1 = np.real(np.exp(1j * np.dot(kVector, bond.vector)))
+            phase_k2 = np.real(np.exp(1j * np.dot(kVector, -bond.vector)))
             H = X_ab * np.exp(-2.7 * f_ab)
 
-            if ID1 == ID2:
-                if not np.isclose(bond.direction, np.zeros(3)).all():
-                    D = AddDynMatSelf(D, ID1, H, C, phase_k1)
+            if i1 == i2:
+                if not np.allclose(bond.direction, [0,0,0]):
+                    D = AddDynMatSelf(D, i1, H, C, phase_k1)
             else:
-                D = AddDynMat(D, ID1, ID2, H, C, phase_k1, phase_k2)
+                D = AddDynMat(D, i1, i2, H, C, phase_k1, phase_k2)
 
     # Compare D from Matlab and Python:
     '''