fix bonds calculation, refactor things

Atomistic/AtomisticConfig.py
@@ -58,7 +58,7 @@ class ChemicalConfig(Config):
     minDistMatrice = None
     CenterminDistMatrice = None
 
-    def __init__(self, symbols : list, volumeType : str=None, ionDistances : dict=None, goodBonds : list=None,
+    def __init__(self, symbols : list, volumeType : str=None, ionDistances : dict=None, goodBonds : dict=None,
                        valences : Dict[str, float]=None, minVectorLength : dict=None, valenceElectrons : Dict[str, float]=None,
                        externalPressure : float=0.0001, moleculesDistinctCheck = True, MolCenters : list=None, **kwargs):
         '''
@@ -101,10 +101,15 @@ class ChemicalConfig(Config):
 
 
         if goodBonds is not None:
-            self.goodBonds = np.asarray(goodBonds)
-            assert len(self.goodBonds.shape) == 2
+            self.goodBonds = goodBonds
         else:
-            self.goodBonds = defaultGoodBonds(self.chemicalSymbols)
+            self.goodBonds = {}
+            goodBonds = defaultGoodBonds(self.chemicalSymbols)
+            for i, j in combinations_with_replacement(range(len(self.chemicalSymbols)), 2):
+                arg = '{}-{}'.format(self.chemicalSymbols[i],self.chemicalSymbols[j])
+                self.goodBonds[arg] = goodBonds[i,j]
+                arg = '{}-{}'.format(self.chemicalSymbols[j], self.chemicalSymbols[i])
+                self.goodBonds[arg] = goodBonds[j, i]
 
         self.valences = valences if valences is not None else {symbol: Element(symbol).valence for symbol in self.chemicalSymbols}
         self.valenceElectrons = valenceElectrons if valenceElectrons is not None else {symbol: Element(symbol).valence_electrons for symbol in self.chemicalSymbols}

Atomistic/Bonds.py
@@ -114,68 +114,100 @@ class Bond(object):
         return not (isEqual_Symbols and isEqualDistance)
 
 
-class Bonds(List[Bond]):
+# class Bonds(List[Bond]):
+#     '''
+#     Class for the list of the bonds.
+#     '''
+#
+#     def append(self, object : Bond):
+#         super(Bonds, self).append(object)
+#
+#     def getType(self, type):
+#         '''
+#         :param type (int or None): type number.
+#         :return: all bonds of _type_.
+#         '''
+#         return Bonds([x for x in self if x.type == type])
+#
+#     def all_types(self):
+#         '''
+#         :return: all types of the bonds in this list.
+#         '''
+#         return set([x.type for x in self])
+#
+#     # TODO do it
+#     def _is3Dconnected(self) -> bool:
+#         X = np.max([np.abs(bond.direction[0]) for bond in self])
+#         Y = np.max([np.abs(bond.direction[1]) for bond in self])
+#         Z = np.max([np.abs(bond.direction[2]) for bond in self])
+#         return X > 0 and Y > 0 and Z > 0
+#
+#     # TODO fix it
+#     def connectList(self):
+#         '''
+#         IMPORTANT NOTE: this implementation checks connectivity of the atoms INSIDE unit cell.
+#         It skips bonds that have and end in neighboring cells.
+#         :return: the connectivity list in this set of bonds.
+#         '''
+#
+#         # First, lets check whether we are not consider the case, when we have only 1 atom in unit cell.
+#         uniqueAtoms = np.unique([bond.indicies for bond in self])
+#         if 1 == len(uniqueAtoms):
+#             return [uniqueAtoms[0]]
+#
+#         # bonds inside the unit cell
+#         bonds = [bond.indicies for bond in self]
+#         uniqueAtoms = np.unique(bonds)
+#         pairs = {}
+#
+#         for atom in uniqueAtoms:
+#             ToAdd = np.zeros((0, 1), dtype=int)
+#             for eachBond in bonds:
+#                 if eachBond[0] == atom:
+#                     ToAdd = np.append(ToAdd, eachBond[1])
+#                 elif eachBond[1] == atom:
+#                     ToAdd = np.append(ToAdd, eachBond[0])
+#             if len(ToAdd):
+#                 ToAdd = np.array([x for x in ToAdd if x != atom], dtype=int)
+#                 pairs[atom] = set(np.unique(ToAdd))
+#
+#         visited = dfs1(pairs, uniqueAtoms[0])
+#         return list(visited)
+#
+#     def __add__(self, other):
+#         return Bonds(super(Bonds,self).__add__(other))
+
+def connectList(bondsGraph):
     '''
-    Class for the list of the bonds.
+    IMPORTANT NOTE: this implementation checks connectivity of the atoms INSIDE unit cell.
+    It skips bonds that have and end in neighboring cells.
+    :return: the connectivity list in this set of bonds.
     '''
+    bonds = [bond.indicies for bond in bondsGraph]
 
-    def append(self, object : Bond):
-        super(Bonds, self).append(object)
+    # First, lets check whether we are not consider the case, when we have only 1 atom in unit cell.
+    uniqueAtoms = np.unique(bonds)
+    if 1 == len(uniqueAtoms):
+        return [uniqueAtoms[0]]
 
-    def getType(self, type):
-        '''
-        :param type (int or None): type number.
-        :return: all bonds of _type_.
-        '''
-        return Bonds([x for x in self if x.type == type])
+    # bonds inside the unit cell
+    uniqueAtoms = np.unique(bonds)
+    pairs = {}
 
-    def all_types(self):
-        '''
-        :return: all types of the bonds in this list.
-        '''
-        return set([x.type for x in self])
+    for atom in uniqueAtoms:
+        ToAdd = np.zeros((0, 1), dtype=int)
+        for eachBond in bonds:
+            if eachBond[0] == atom:
+                ToAdd = np.append(ToAdd, eachBond[1])
+            elif eachBond[1] == atom:
+                ToAdd = np.append(ToAdd, eachBond[0])
+        if len(ToAdd):
+            ToAdd = np.array([x for x in ToAdd if x != atom], dtype=int)
+            pairs[atom] = set(np.unique(ToAdd))
 
-    # TODO do it
-    def _is3Dconnected(self) -> bool:
-        X = np.max([np.abs(bond.direction[0]) for bond in self])
-        Y = np.max([np.abs(bond.direction[1]) for bond in self])
-        Z = np.max([np.abs(bond.direction[2]) for bond in self])
-        return X > 0 and Y > 0 and Z > 0
+    visited = dfs1(pairs, uniqueAtoms[0])
+    return list(visited)
 
-    # TODO fix it
-    def connectList(self):
-        '''
-        IMPORTANT NOTE: this implementation checks connectivity of the atoms INSIDE unit cell.
-        It skips bonds that have and end in neighboring cells.
-        :return: the connectivity list in this set of bonds.
-        '''
-
-        # First, lets check whether we are not consider the case, when we have only 1 atom in unit cell.
-        uniqueAtoms = np.unique([bond.atoms() for bond in self])
-        if 1 == len(uniqueAtoms):
-            return [uniqueAtoms[0]]
-
-        # bonds inside the unit cell
-        bonds = [bond.atoms() for bond in self if np.isclose(bond.direction, [0,0,0]).all()]
-        uniqueAtoms = np.unique(bonds)
-        pairs = {}
-
-        for atom in uniqueAtoms:
-            ToAdd = np.zeros((0, 1), dtype=int)
-            for eachBond in bonds:
-                if eachBond[0] == atom:
-                    ToAdd = np.append(ToAdd, eachBond[1])
-                elif eachBond[1] == atom:
-                    ToAdd = np.append(ToAdd, eachBond[0])
-            if len(ToAdd):
-                ToAdd = np.array([x for x in ToAdd if x != atom], dtype=int)
-                pairs[atom] = set(np.unique(ToAdd))
-
-        visited = dfs1(pairs, uniqueAtoms[0])
-        return list(visited)
-
-    def __add__(self, other):
-        return Bonds(super(Bonds,self).__add__(other))
 
 
 def defaultGoodBonds(symbols):

Atomistic/softmodes_new/BondHardness_new.py
@@ -1,10 +1,10 @@
 import numpy as np
+from itertools import chain
 
 
 from USPEX.Common.Atomistic.AtomicStructure import AtomicStructure
-from ..Bonds import Bonds
-from .MaxBonds import MaxBonds
-from .AtomTypeCounter import atomTypeCounter
+from ..Bonds import connectList
+from .MaxBonds import MaxBonds_new
 
 
 _BONDS_CUTOFF = 5.0     # Angstroms
@@ -13,7 +13,7 @@ _BONDS_CUTOFF = 5.0     # Angstroms
 
 
 
-def BondHardness_new(SYSTEM : AtomicStructure, goodBonds) -> Bonds:
+def BondHardness_new(SYSTEM : AtomicStructure, goodBonds) -> list:
     '''
     The function calculates bonds which make contribution to hardness.
     Used only for softmodemutation case and does not used for any other cases.
@@ -28,31 +28,25 @@ def BondHardness_new(SYSTEM : AtomicStructure, goodBonds) -> Bonds:
     :return:
     '''
 
-    small_bond = -0.37 * np.log(np.array(goodBonds))  # maximum deviation to consider a bond
-
     N_atom = len(SYSTEM)
-    atomTypes, atom_type_seq = atomTypeCounter(SYSTEM.chemicalSymbols)
+
 
     # 1) Calculate bonds within upper bound to max_bond.
     # 2) Group bonds by using same_bond criterion.
-    bond_total = MaxBonds(SYSTEM)
-    bond_group = np.unique([bond.type for bond in bond_total]).tolist()
-    bond_group_ref = np.copy(bond_group)
+    bond_total = MaxBonds_new(SYSTEM)
 
     # 3) Add bonds by group.
-    bond_in = Bonds()
+    bond_in = []
+    bond_left = []
 
     # delete short bonds
-    id = 1
-    ToDelete = []
-    while id < len(bond_group_ref):
-        bonds = bond_total.getType(id)
-        a,b = bonds[0].atoms()
-        a,b = atom_type_seq[a], atom_type_seq[b]
-        if min([b.delta for b in bonds]) < small_bond[a,b]:
-            bond_in.extend(bonds)    # Add by group
-            ToDelete.append(id)
-        id = id + 1
+    for bonds in bond_total:
+        a,b = bonds[0].symbols
+        small_bond = -0.37 * np.log(goodBonds[f'{a}-{b}'])
+        if min([bond.delta for bond in bonds]) < small_bond:
+            bond_in.append(bonds)    # Add by group
+        else:
+            bond_left.append(bonds)
     # del bond_group[0]
 
     # 5, check 3D connectivity, if not satisfied, add more bonds
@@ -60,34 +54,30 @@ def BondHardness_new(SYSTEM : AtomicStructure, goodBonds) -> Bonds:
     # ---Looks like we have to include all bonds before the connectivity changes
     #   otherwise, we won't add them
 
-    List = bond_in.connectList()
+    List = connectList(chain(*bond_in))
 
     while len(List) < N_atom:
         # disp('The stuture is not fully connected, adding more bonds');
-        id = bond_group[0]
-        if id not in ToDelete:
-            bonds = bond_total.getType(id)
-            bond_tmp = bond_in + bonds
-            List_new = bond_tmp.connectList()
-            del bond_group[0]
-            if len(List_new) > len(List) or len(List) == 1: # increase connectivity accept
-                # disp('The connectivity is increased, accept adding more bonds');
-                List = List_new
-                bond_in = bond_tmp
-                # else
-                # disp('The connectivity is not increased, reject adding more bonds');
-        else:
-            if len(bond_group):
-                del bond_group[0]
+        bonds = bond_left[0]
+        bond_tmp = bond_in + [bonds]
+        List_new = connectList(chain(*bond_tmp))
+        del bond_left[0]
+        if len(List_new) > len(List) or len(List) == 1: # increase connectivity accept
+            # disp('The connectivity is increased, accept adding more bonds');
+            List = List_new
+            bond_in = bond_tmp
+            # else
+            # disp('The connectivity is not increased, reject adding more bonds');
 
     # 6, Remove double count of bond like [i,i] pair;
-    indicies = []
-    for i, bond in enumerate(bond_in):
-        a,b = bond.atoms()
-        if a == b:
-            indicies.append(i)
-    for i in sorted(indicies[::2], reverse=True):
-        del bond_in[i]
+    for i, bonds_tmp in enumerate(bond_in):
+        indicies = []
+        for j, bond in enumerate(bonds_tmp):
+            a,b = bond.indicies
+            if a == b:
+                indicies.append(j)
+        for j in sorted(indicies[::2], reverse=True):
+            del bond_in[i][j]
 
     return bond_in
 

Atomistic/softmodes_new/Softmodes.py
@@ -4,14 +4,14 @@ import warnings
 
 import numpy as np
 
-from USPEX.Common.Config import Config
+from USPEX.Common.Atomistic.AtomisticConfig import AtomisticConfig
 from USPEX.Common.Atomistic.Element import Element
 from USPEX.Common.Atomistic.AtomicStructure import AtomicStructure
 
 from USPEX.Common.Atomistic.Bonds import Bonds
 from .BondHardness_new import BondHardness_new
-from .MaxBonds import MaxBonds, MaxBonds_new
-from .calcHardness import calcHardness
+from .MaxBonds import MaxBonds_new
+from .calcHardness import calcHardness_new
 from .calcSoftModes import calcSoftModes
 from .AtomTypeCounter import atomTypeCounter
 
@@ -39,7 +39,7 @@ class Softmodes(list):
 
     duration_h = -np.inf
 
-    def __init__(self, config : Config, system : AtomicStructure, kvector : List[int]=np.zeros(3)):
+    def __init__(self, config : AtomisticConfig, system : AtomicStructure, kvector : List[int]=np.zeros(3)):
         '''
 
         :param config:
@@ -67,32 +67,17 @@ class Softmodes(list):
         #     self.N_val = [Element(x).valence_electrons for x in atomTypes]
         # self.R_val = np.array([Element(atomType).covalent_radius for atomType in atomTypes])
 
-        start_time = time.time()
-        # bonds = MaxBonds(system)
-        self.duration_bh = time.time() - start_time
-        start_time = time.time()
-        bonds_new = MaxBonds_new(system)
-        self.duration_bh_new = time.time() - start_time
-        # self._calc_soft_modes(system, bonds, kvector)
-        self._calc_soft_modes(system, bonds_new, kvector)
+        frequencies, eigenvectors = calcSoftModes(system, self._config, kvector)
+        self.extend([VibrationalMode(f, v) for f, v in zip(frequencies, eigenvectors.T)])
 
     def hardness(self):
         '''
         :return: hardness of the material.
         '''
         start_time = time.time()
-        hardness = calcHardness(self._config, self.system)
+        hardness = calcHardness_new(self._config, self.system)
         self.duration_h = time.time() - start_time
         return hardness
-
-    def _calc_soft_modes(self, system, bonds:Bonds, kvector : List[int]=np.zeros(3)):
-        start_time = time.time()
-        # each eigenvector has written as column. So, next we are using transpose matrix.
-        # self.R_val, self.N_val, self.val,
-        frequencies, eigenvectors = calcSoftModes(system, self._config, bonds, kvector)
-        self.extend([VibrationalMode(f, v) for f, v in zip(frequencies, eigenvectors.T)])
-        self.duration_sm = time.time() - start_time
-
 
 #
 # if __name__ == '__main__':

Atomistic/softmodes_new/calcSoftModes.py
@@ -1,7 +1,7 @@
 from __future__ import division
 
 from USPEX.Common.Atomistic.AtomicStructure import AtomicStructure
-from USPEX.Common.Atomistic.Bonds import Bonds
+from .BondHardness_new import BondHardness_new
 
 from USPEX.Common.Atomistic.Element import Element
 
@@ -9,6 +9,7 @@ from USPEX.Common.Atomistic.Element import Element
 
 
 import numpy as np
+from itertools import chain
 
 
 R_val = lambda symbol: Element(symbol).covalent_radius
@@ -42,7 +43,7 @@ def AddDynMat(D, a, b, H, Cos, phase1, phase2):
 
 
 # R_val : dict, N_val : dict, val : dict,
-def calcSoftModes(system : AtomicStructure, config, bonds, kVector0=np.zeros(3)):
+def calcSoftModes(system : AtomicStructure, config, kVector0=np.zeros(3)):
     '''
     The function calculates vibrational modes based on the dynamic matrix (D) constructed from bond hardness model.
 
@@ -59,6 +60,8 @@ def calcSoftModes(system : AtomicStructure, config, bonds, kVector0=np.zeros(3))
     :return eigvector: eigenvector of all modes.
     '''
 
+    bonds = BondHardness_new(system, config.goodBonds)
+
     # assert isinstance(system.bonds, Bonds)
 
     # Convert everything to ndarray:
@@ -73,8 +76,6 @@ def calcSoftModes(system : AtomicStructure, config, bonds, kVector0=np.zeros(3))
     kVector = np.dot(kVector0, rec_lat)
 
     # Obtain the bond information:
-    bond_groups = bonds.all_types()    # list of bond group
-    N_group = len(bond_groups)           # number of bond group
     N_atom = len(system)  # number of atoms
 
     # Initiallization of Dynamic matrix (3N*3N):
@@ -82,21 +83,22 @@ def calcSoftModes(system : AtomicStructure, config, bonds, kVector0=np.zeros(3))
 
     # Calculate bond valence using classical Brown's bond valence model.
     # nu_factor should be normalized to satisfy sum rule.
-    nu_factor = np.zeros(N_atom)
+    nu_factor = []
 
     # atomTypes, atom_type_seq = atomTypeCounter(system.chemicalSymbols)
-    for k, s in zip(range(N_atom), system.get_chemical_symbols()):
+    for k, symbol in enumerate(system.chemicalSymbols):
         nu_full = 0.0
-        for bond in bonds:
+
+        for bond in chain(*bonds):  # how many type of bonds
             a, b = bond.indicies
             if a == k:
                 nu_full += np.exp(-bond.delta / 0.37)
             if b == k:
                 nu_full += np.exp(-bond.delta / 0.37)
-        nu_factor[k] = config.valences[s] / nu_full
+        nu_factor.append(config.valences[symbol] / nu_full)
 
-    for bondtype in bond_groups:
-        for bond in bonds.getType(bondtype):
+    for bond_group in bonds:
+        for bond in bond_group:
             s1, s2 = bond.symbols
             i1, i2 = bond.indicies
             # a = atom_type_seq[ID1]

Atomistic/softmodes_new/tests/SoftModes_test.py
@@ -7,7 +7,7 @@ from ase.io.vasp import read_vasp
 from os.path import join as pj
 
 from ...AtomisticConfig import AtomisticConfig
-from ..Softmodes import Softmodes
+from ..calcSoftModes import calcSoftModes
 from ...AtomicStructure import AtomicStructure
 from ...Crystal import Crystal
 
@@ -28,10 +28,7 @@ class SoftModes_test(unittest.TestCase):
                          symbols=tmp.get_chemical_symbols())
         params = {'symbols': ['Mg', 'Al', 'O'], 'blocks': [[4, 8, 16]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
-        softmodes = Softmodes(config, system)
-        print(f'All necessary bonds estimation time = {softmodes.duration_bh_new}')
-        print(f'Vibration modes estimation time = {softmodes.duration_sm}')
-        print('==========================')
+        freq, eigvector = calcSoftModes(system, config)
 
     def test_MgAlO_system_2(self):
         print('Test MgAlO 2')
@@ -41,10 +38,7 @@ class SoftModes_test(unittest.TestCase):
                          symbols=tmp.get_chemical_symbols())
         params = {'symbols': ['Mg', 'Al', 'O'], 'blocks': [[4, 8, 16]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
-        softmodes = Softmodes(config, system)
-        print(f'All necessary bonds estimation time = {softmodes.duration_bh_new}')
-        print(f'Vibration modes estimation time = {softmodes.duration_sm}')
-        print('==========================')
+        freq, eigvector = calcSoftModes(system, config)
 
     def test_MgAlO_system_3(self):
         print('Test MgAlO 3')
@@ -54,10 +48,7 @@ class SoftModes_test(unittest.TestCase):
                          symbols=tmp.get_chemical_symbols())
         params = {'symbols': ['Mg', 'Al', 'O'], 'blocks': [[4, 8, 16]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
-        softmodes = Softmodes(config, system)
-        print(f'All necessary bonds estimation time = {softmodes.duration_bh_new}')
-        print(f'Vibration modes estimation time = {softmodes.duration_sm}')
-        print('==========================')
+        freq, eigvector = calcSoftModes(system, config)
 
     # Carbon systems
     def test_graphite(self):
@@ -68,15 +59,12 @@ class SoftModes_test(unittest.TestCase):
         N = len(graphite)
         params = {'symbols' : ['C'], 'blocks' : [[N]], 'fixed' : [[1, 1]]}
         config = AtomisticConfig(**params)
-        softmodes = Softmodes(config, graphite)
-        print(f'All necessary bonds estimation time = {softmodes.duration_bh_new}')
-        print(f'Vibration modes estimation time = {softmodes.duration_sm}')
+        freq, eigvector = calcSoftModes(graphite, config)
 
         freq_ref = [-3.14018492e-16, -2.07235140e-17, 0.00000000e+00, 0.00000000e+00, 2.74458612e-23, 7.54840750e-17,
                      1.78390610e-16, 8.28730183e-03, 2.70874463e+00,  2.70874473e+00,  2.70892967e+00,  2.70893071e+00]
 
-        # assert np.isclose([mode.frequency for mode in softmodes], freq_ref).all()
-        print('==========================')
+        self.assertTrue(np.allclose(freq, freq_ref))
 
 
     def test_graphite_supercell(self):
@@ -88,9 +76,7 @@ class SoftModes_test(unittest.TestCase):
         N = len(graphite)
         params = {'symbols' : ['C'], 'blocks' : [[N]], 'fixed' : [[1,1]]}
         config = AtomisticConfig(**params)
-        softmodes = Softmodes(config, graphite)
-        print(f'All necessary bonds estimation time = {softmodes.duration_bh_new}')
-        print(f'Vibration modes estimation time = {softmodes.duration_sm}')
+        freq, eigvector = calcSoftModes(graphite, config)
 
         freq_ref = [ -6.88734374e-16 , -5.81698639e-16 , -5.81698639e-16 , -3.31208915e-16,
                      -2.44283384e-16 , -2.44283384e-16 , -2.00449320e-16 , -2.00449320e-16,
@@ -117,8 +103,7 @@ class SoftModes_test(unittest.TestCase):
                       2.70888357e+00 ,  2.70888357e+00 ,  2.70888481e+00 ,  2.70888481e+00,
                       2.70892967e+00 ,  2.70892967e+00 ,  2.70893071e+00 ,  2.70893071e+00]
 
-        # assert np.isclose([mode.frequency for mode in softmodes], freq_ref).all()
-        print('==========================')
+        self.assertTrue(np.allclose(freq, freq_ref))
 
 
     def test_graphite2(self):
@@ -129,15 +114,12 @@ class SoftModes_test(unittest.TestCase):
         N = len(graphite)
         params = {'symbols' : ['C'], 'blocks' : [[N]], 'fixed' : [[1,1]]}
         config = AtomisticConfig(**params)
-        softmodes = Softmodes(config, graphite)
-        print(f'All necessary bonds estimation time = {softmodes.duration_bh_new}')
-        print(f'Vibration modes estimation time = {softmodes.duration_sm}')
+        freq, eigvector = calcSoftModes(graphite, config)
 
         freq_ref = [-3.14018492e-16, -2.07235140e-17, 0.00000000e+00, 0.00000000e+00, 2.74458612e-23, 7.54840750e-17,
                      1.78390610e-16, 8.28730183e-03, 2.70874463e+00,  2.70874473e+00,  2.70892967e+00,  2.70893071e+00]
 
-        # assert np.isclose([mode.frequency for mode in softmodes], freq_ref).all()
-        print('==========================')
+        self.assertTrue(np.allclose(freq, freq_ref))
 
     def test_graphite2_supercell(self):
         print('Test graphite supercell 2')
@@ -148,15 +130,12 @@ class SoftModes_test(unittest.TestCase):
         N = len(graphite)
         params = {'symbols' : ['C'], 'blocks' : [[N]], 'fixed' : [[1,1]]}
         config = AtomisticConfig(**params)
-        softmodes = Softmodes(config, graphite)
-        print(f'All necessary bonds estimation time = {softmodes.duration_bh_new}')
-        print(f'Vibration modes estimation time = {softmodes.duration_sm}')
+        freq, eigvector = calcSoftModes(graphite, config)
 
         freq_ref = [-3.14018492e-16, -2.07235140e-17, 0.00000000e+00, 0.00000000e+00, 2.74458612e-23, 7.54840750e-17,
                      1.78390610e-16, 8.28730183e-03, 2.70874463e+00,  2.70874473e+00,  2.70892967e+00,  2.70893071e+00]
 
-        # assert np.isclose([mode.frequency for mode in softmodes], freq_ref).all()
-        print('==========================')
+        self.assertTrue(np.allclose(freq, freq_ref))
 
 
     def test_diamond(self):
@@ -167,14 +146,9 @@ class SoftModes_test(unittest.TestCase):
         N = len(diamond)
         params = {'symbols' : ['C'], 'blocks' : [[N]], 'fixed' : [[1,1]]}
         config = AtomisticConfig(**params)
-        softmodes = Softmodes(config, diamond)
-        print(f'All necessary bonds estimation time = {softmodes.duration_bh_new}')
-        print(f'Vibration modes estimation time = {softmodes.duration_sm}')
-
-        freq = [mode.frequency for mode in softmodes]
+        freq, eigvector = calcSoftModes(diamond, config)
         freq_ref = [ -2.22044605e-16, -2.22044605e-16, -2.22044605e-16,  1.66088535e+00, 1.66088535e+00,  1.66088535e+00]
-        # assert np.isclose(freq, freq_ref).all()
-        print('==========================')
+        self.assertTrue(np.allclose(freq, freq_ref))
 
 
     def test_diamond_supercell(self):
@@ -186,7 +160,7 @@ class SoftModes_test(unittest.TestCase):
         N = len(diamond)
         params = {'symbols' : ['C'], 'blocks' : [[N]], 'fixed' : [[1,1]]}
         config = AtomisticConfig(**params)
-        softmodes = Softmodes(config, diamond)
+        freq, eigvector = calcSoftModes(diamond, config)
 
         freq_ref = [ -4.81633103e-16,  -2.68342179e-16 , -2.38233999e-16 , -2.38233999e-16,
                      -2.07559197e-16,  -2.07559197e-16 , -9.21082950e-17 , -9.21082950e-17,
@@ -200,8 +174,7 @@ class SoftModes_test(unittest.TestCase):
                       1.66088535e+00,   1.66088535e+00 ,  1.66088535e+00 ,  1.66088535e+00,
                       1.66088535e+00,   1.66088535e+00 ,  1.66088535e+00 ,  1.66088535e+00,
                       1.66088535e+00,   1.66088535e+00 ,  1.66088535e+00 ,  1.66088535e+00]
-        # assert np.isclose([mode.frequency for mode in softmodes], freq_ref).all()
-        print('==========================')
+        self.assertTrue(np.allclose(freq, freq_ref))
 
     # MgO-systems
 
@@ -213,13 +186,7 @@ class SoftModes_test(unittest.TestCase):
         params = {'symbols': ['Mg', 'O'], 'blocks': [[4, 4]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
 
-        softmodes = Softmodes(config, system)
-        print(f'All necessary bonds estimation time = {softmodes.duration_bh_new}')
-        print(f'Vibration modes estimation time = {softmodes.duration_sm}')
-
-        print('MgO_1')
-        print('==========================')
-
+        freq, eigvector = calcSoftModes(system, config)
 
     def test_MgO_2(self):
         print('Test MgO 2')
@@ -230,6 +197,4 @@ class SoftModes_test(unittest.TestCase):
         params = {'symbols': ['Mg', 'O'], 'blocks': [[1, 1]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
 
-        softmodes = Softmodes(config, system)
-        print('MgO_2')
-        print('==========================')
+        freq, eigvector = calcSoftModes(system, config)

Atomistic/softmodes_new/tests/hardness_test.py
@@ -9,7 +9,7 @@ from ase.io.vasp import write_vasp
 
 from ...AtomisticConfig import AtomisticConfig
 from ...Crystal import Crystal
-from ..calcHardness import calcHardness, calcHardness_new
+from ..calcHardness import calcHardness_new
 
 
 class Hardness_test(unittest.TestCase):
@@ -26,9 +26,9 @@ class Hardness_test(unittest.TestCase):
         graphite = Crystal(symbols=symbols, scaled_positions=tmp.get_scaled_positions(), cell=tmp.get_cell())
         params = {'symbols': ['C'], 'blocks': [[len(graphite)]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
-        H = calcHardness(config, graphite)
+        H = calcHardness_new(config, graphite)
         print('Hardness = ' + str(H))
-        assert np.isclose(H, 0.231, atol=1)
+        self.assertAlmostEqual(H, 0.231, places=3)
 
     def test_graphite2(self):
         tmp = read_vasp(self.CURRENT_DIR + '/graphite2.POSCAR')
@@ -36,9 +36,9 @@ class Hardness_test(unittest.TestCase):
         graphite = Crystal(symbols=symbols, scaled_positions=tmp.get_scaled_positions(), cell=tmp.get_cell())
         params = {'symbols': ['C'], 'blocks': [[len(graphite)]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
-        H = calcHardness(config, graphite)
+        H = calcHardness_new(config, graphite)
         print('Hardness = ' + str(H))
-        assert np.isclose(H, 0.433, atol=1)
+        self.assertAlmostEqual(H, 0.433, places=3)
 
     def test_graphite2_supercell(self):
         tmp = read_vasp(self.CURRENT_DIR + '/graphite2.POSCAR')
@@ -47,19 +47,20 @@ class Hardness_test(unittest.TestCase):
         graphite *= 3
         params = {'symbols': ['C'], 'blocks': [[len(graphite)]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
-        H = calcHardness(config, graphite)
+        H = calcHardness_new(config, graphite)
         print('Hardness = ' + str(H))
-        assert np.isclose(H, 0.433, atol=1)
+        self.assertAlmostEqual(H, 0.433, places=3)
 
-    def test_graphite_1layer(self):
-        tmp = read_vasp(self.CURRENT_DIR + '/graphite_1layer.POSCAR')
-        symbols = tmp.get_chemical_symbols()
-        graphite = Crystal(symbols=symbols, scaled_positions=tmp.get_scaled_positions(), cell=tmp.get_cell())
-        params = {'symbols': ['C'], 'blocks': [[len(graphite)]], 'fixed': [[1, 1]]}
-        config = AtomisticConfig(**params)
-        H = calcHardness(config, graphite)
-        print('Hardness = ' + str(H))
-        assert np.isclose(H, 1.72, atol=1)
+    # TODO this is not Crystal case, need new AtomicStructure subclass
+    # def test_graphite_1layer(self):
+    #     tmp = read_vasp(self.CURRENT_DIR + '/graphite_1layer.POSCAR')
+    #     symbols = tmp.get_chemical_symbols()
+    #     graphite = Crystal(symbols=symbols, scaled_positions=tmp.get_scaled_positions(), cell=tmp.get_cell())
+    #     params = {'symbols': ['C'], 'blocks': [[len(graphite)]], 'fixed': [[1, 1]]}
+    #     config = AtomisticConfig(**params)
+    #     H = calcHardness_new(config, graphite)
+    #     print('Hardness = ' + str(H))
+    #     self.assertAlmostEqual(H, 1.72, places=3)
 
     def test_aluminium(self):
         tmp = read_vasp(self.CURRENT_DIR + '/al.POSCAR')
@@ -67,9 +68,9 @@ class Hardness_test(unittest.TestCase):
         system = Crystal(symbols=symbols, scaled_positions=tmp.get_scaled_positions(), cell=tmp.get_cell())
         params = {'symbols': ['Al'], 'blocks': [[len(system)]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
-        H = calcHardness(config, system)
+        H = calcHardness_new(config, system)
         print('Hardness = ' + str(H))
-        assert np.isclose(H, 10.562, atol=1)
+        self.assertAlmostEqual(H, 10.562, places=3)
 
     def test_diamond(self):
         tmp = read_vasp(self.CURRENT_DIR + '/diamond.POSCAR')
@@ -77,9 +78,9 @@ class Hardness_test(unittest.TestCase):
         diamond = Crystal(symbols=symbols, scaled_positions=tmp.get_scaled_positions(), cell=tmp.get_cell())
         params = {'symbols': ['C'], 'blocks': [[len(diamond)]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
-        H = calcHardness(config, diamond)
+        H = calcHardness_new(config, diamond)
         print('Hardness = ' + str(H))
-        assert np.isclose(H, 89.656, atol=1)
+        self.assertAlmostEqual(H, 89.656, places=3)
 
     def test_Mg4Al8O16(self):
         tmp = read_vasp(self.CURRENT_DIR + '/Mg4Al8O16.POSCAR')
@@ -89,9 +90,9 @@ class Hardness_test(unittest.TestCase):
         # write_vasp('tmp.POSCAR', system, direct=True, vasp5=True, sort=True)
         params = {'symbols': ['Mg', 'Al', 'O'], 'blocks': [[4, 8, 16]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
-        H = calcHardness(config, system)
+        H = calcHardness_new(config, system)
         print('Hardness = ' + str(H))
-        assert np.isclose(H, 6.177, atol=1)
+        self.assertAlmostEqual(H, 6.177, places=3)
 
     def test_Mg4Al8O16_2(self):
         tmp = read_vasp(self.CURRENT_DIR + '/Mg4Al8O16_2.POSCAR')
@@ -100,6 +101,6 @@ class Hardness_test(unittest.TestCase):
         #write_vasp('tmp.vasp', system, vasp5=True, sort=True)
         params = {'symbols': ['Mg', 'Al', 'O'], 'blocks': [[4, 8, 16]], 'fixed': [[1, 1]]}
         config = AtomisticConfig(**params)
-        H = calcHardness(config, system)
+        H = calcHardness_new(config, system)
         print('Hardness = ' + str(H))
-        assert np.isclose(H, 19.970, atol=1)
+        self.assertAlmostEqual(H, 19.970, places=0)