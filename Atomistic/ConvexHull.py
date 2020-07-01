3507ae742ba02fb71b9753bb88d86c047be54ff8
Artem Samtsevich <samtsevichartem@gmail.com>
2020-07-01 20:32:07 +0000

    Merge branch 'master' of git.uspex-team.org:uspex_python/common into 42-implement-generalized-convex-hull

--- Atomistic/ConvexHull.py
+++ Atomistic/ConvexHull.py
@@ -15,12 +15,202 @@ FunctionFolder/USPEX/301/extendedConvexHull_301.m
 FunctionFolder/USPEX/src/CheckDecomposition.m
 '''
 
-import itertools
+import logging
+logger = logging.getLogger(__name__)
+
+
 import numpy as np
-import copy
+import os
+import pandas as pd
+
+from copy import copy
+from itertools import chain, combinations
+from scipy.spatial import ConvexHull as QHull
+from typing import List, Tuple
 
 
-class ConvexHull(object):
+
+class Simplex:
+    '''
+    Description of n-dimensional simplex on a given set of points
+    '''
+
+    def __init__(self, coords):
+        self._coords = np.array(coords)
+        self.space_dim, self.simplex_dim = self._coords.shape
+        self.origin = self._coords[-1]
+        if self.space_dim == self.simplex_dim + 1:
+            self._aug = np.concatenate([coords, np.ones((self.space_dim, 1))], axis=-1)
+            self._aug_inv = np.linalg.inv(self._aug)
+
+    def bary_coords(self, point):
+        try:
+            return np.dot(np.concatenate([point, [1]]), self._aug_inv)
+        except AttributeError:
+            raise ValueError('Error: simplex is not full-dimensional!')
+
+
+class ConvexHull:
+
+    # ID of systems on the convex hull
+    # _systems = []
+    # all_IDs = []
+
+    def __init__(self, saved_data : str, property:str='enthalpy_per_atom'):
+        self._df = pd.DataFrame(columns=['ID', 'principal_component', 'property', 'height', 'depth'])
+        self.property = property
+
+        # self._df = pd.DataFrame(columns=['ID', 'enthalpy_per_atom', 'fingerprint', 'height'])
+        self.SAVED_DATAFILE = saved_data
+
+    def extend(self, systems):
+        if not len(systems):
+            return
+        if os.path.exists(self.SAVED_DATAFILE):
+            self._df = pd.read_pickle(self.SAVED_DATAFILE)
+
+        # ID of all systems in database
+        all_IDs = self._df.ID.tolist()
+
+        for ID, system in systems:
+            if ID not in all_IDs:
+                self._df.loc[len(self._df)] = ID, system.principal_component, getattr(system, self.property), np.inf, -np.inf
+
+        properties = self._df.property.tolist()
+        coords = self._df.principal_component.tolist()
+
+        min_ID, max_ID = np.argmin(properties), np.argmax(properties)
+
+        principal_component_set = np.column_stack((properties, coords))
+        # m - number of structures
+        # n - size of principal components
+        m,n = principal_component_set.shape
+
+
+        if n == 1:  # Single component
+            min_E, max_E = np.min(properties), np.max(properties)
+            for i, row in self._df.iterrows():
+                self._df.at[i, 'height'] = row.property - min_E
+                self._df.at[i, 'depth'] = row.property - max_E
+        elif m <= n:    # Not enough point to build proper CH, so all structures are on CH
+            for i, _ in self._df.iterrows():
+                self._df.at[i, 'height'] = 0.0
+                self._df.at[i, 'depth'] = 0.0
+        else:
+            qhull = QHull(principal_component_set)
+
+            assert min_ID in qhull.vertices, f'Structure with ID={min_ID} has lowest energy and must be on the CH.'
+            assert max_ID in qhull.vertices, f'Structure with ID={max_ID} has highest energy and must be on the CH.'
+
+            simplecies = []
+            props = []
+            for facet in qhull.simplices:
+                try:
+                    x = Simplex(np.array([coords[x] for x in facet]))
+                    simplecies.append(x)
+                    props.append([properties[x] for x in facet])
+                except:
+                    # This is the case, when Simplex cannot be created due to degenerate input matrix
+                    # For composition case it means 2 or more structures have the same composition, but different energies
+                    continue
+
+            for p, coord, (i, _) in zip(properties, coords, self._df.iterrows()):
+                # This is a dists to simplicies of CH.
+                # Positive when distance to the lower bound, negative when distance to the upper bound.
+                _dists = []
+                for e, s in zip(props, simplecies):
+                    y = s.bary_coords(coord)
+                    if np.abs(np.sum(np.sign(y))) == len(y) or np.any(np.isclose(y, 0.0)):
+                        _dists.append(np.round(p - np.dot(y, e), 6))
+                self._df.at[i, 'height'] = np.max(_dists)
+                self._df.at[i, 'depth'] = np.min(_dists)
+
+            # Check whether structure with lowest property is on CH
+            for v in qhull.vertices:
+                assert np.isclose(self._df.iloc[v].height, 0.0) or np.isclose(self._df.iloc[v].depth, 0.0)
+
+        assert np.isclose(self._df.iloc[min_ID].height, 0.0)
+        assert np.isclose(self._df.iloc[max_ID].depth, 0.0)
+        # assert set(qhull.vertices) == set(chain(self.lower_bound, self.upper_bound))
+
+        self._df.to_pickle(self.SAVED_DATAFILE)
+
+    def clean(self):
+        if os.path.exists(self.SAVED_DATAFILE):
+            os.remove(self.SAVED_DATAFILE)
+
+    @property
+    def lower_bound(self) -> set:
+        '''
+        :return: IDs of structures, which are on the lower bound of CH
+        '''
+        return set(x.ID for _,x in self._df.iterrows() if np.isclose(x.height, 0.0))
+
+    @property
+    def upper_bound(self) -> set:
+        '''
+        :return: IDs of structures, which are on the lower bound of CH
+        '''
+        return set(x.ID for _,x in self._df.iterrows() if np.isclose(x.depth, 0.0))
+
+    def depth(self, ID : int):
+        '''
+        Calculates depth below the top of convex hull
+        : param ID: ID of the structure
+        '''
+        for _, row in self._df.iterrows():
+            if ID == row.ID:
+                return row.depth
+        return -np.inf
+
+    def height(self, ID : int):
+        '''
+        Calculates height above convex hull
+        : param ID: ID of the structure
+        '''
+        for _, row in self._df.iterrows():
+            if ID == row.ID:
+                return row.height
+        return np.inf
+
+
+DEFAULT_DATA_FILE = 'compositionCH.dump'
+
+
+class CompositionConvexHull(ConvexHull):
+    """
+    Convex hull in space of chemical composition and enthalpy. Dictionary-like object, where keys are structures
+    with defined composition and values are energy above convex hull.
+
+    :examples:
+
+    Create empty convex hull object:
+    >>> convex_hull = CompositionConvexHull(config)
+
+    To extend convex hull by set of systems:
+    >>> convex_hull.extend(systems)
+    """
+
+
+    def __init__(self, config, saved_data:str=None):
+        """
+        :type config: :class:`~USPEX.Common.Config.Config` or descendant
+        :param config: describes the chemical compositions configuration space.
+        """
+        if saved_data is None:
+            saved_data = DEFAULT_DATA_FILE
+        super().__init__(saved_data=saved_data, property='enthalpy_per_block')
+        self.config = config
+
+    def extend(self, systems):
+        for ID, system in systems:
+            composition = self.config.numBlocks(system.composition)
+            system.enthalpy_per_block = system.enthalpy/sum(composition)
+            system.principal_component = composition[:-1] / np.sum(composition)
+        super().extend(systems)
+
+
+class ConvexHull_old(object):
     """
     Convex hull in space of chemical composition and enthalpy. Dictionary-like object, where keys are structures
     with defined composition and values are energy above convex hull.
@@ -101,7 +291,7 @@ class ConvexHull(object):
             # when convex hull not yet contain enough systems,
             # we consider formation energy regarding combination of all systems on convex hull
             form_Eng = []
-            for comb in itertools.combinations(range(N_Comp), N_Block):
+            for comb in combinations(range(N_Comp), N_Block):
                 for j in range(N_Block):
                     C[j, :] = composition_hull[comb[j]]
                     C[j, :] /= np.sum(C[j, :])  # normalization
---
+++ Atomistic/GCH.py
@@ -0,0 +1,85 @@
+import logging
+logger = logging.getLogger(__name__)
+
+
+import numpy as np
+
+from itertools import chain, combinations_with_replacement
+
+from .ConvexHull import ConvexHull
+
+
+# To which dimensionality we project our FP and consider
+MAX_DIMENSIONALITY = 7
+
+DEFAULT_DATA_FILE = 'gch.dump'
+
+
+class GeneralizedConvexHull(ConvexHull):
+    '''
+    Class for the building generalized convex hull.
+    It shows which structures are stable and which are not.
+
+    Create empty convex hull object:
+    >>> convex_hull = GeneralizedConvexHull(config)
+
+    Every single generation GCH must be rebuild in the following way:
+    >>> convex_hull.clean()
+    >>> convex_hull.extend(uniqueSystems + population)
+
+    This is due to the projection mechanism which is based on previously found stuctures.
+    '''
+
+    def __init__(self, config, dimensionality : int=None, saved_data : str=None):
+        '''
+        :param dimensionality: dimension of the FP projection
+        :param saved_data: path to file, where will be stored temporary data
+        '''
+        self.config = config
+
+        # Set of unique chemical symbols
+        self._symbols = set(config.chemicalSymbols)
+
+        # If dimension is not set we define it like:
+        if dimensionality is not None:
+            assert dimensionality > 1  # Must be = 1D (energy) + nD (fingerprints projection)
+            self.DIMENSIONALITY = 1+dimensionality  # Energy + FP projection
+        else:
+            d = 2 + len(self._symbols)
+            self.DIMENSIONALITY = np.min([1+d, MAX_DIMENSIONALITY])
+        logger.info(f'Dimensionality of GCH = {self.DIMENSIONALITY}')
+
+        super().__init__(saved_data=saved_data, property='enthalpy_per_block')
+
+    def _flatten_fp(self, fingerprint):
+        '''
+        Flatten fingerprint into a long list by the follows rule:
+        :param fp_value: value of the FP
+        '''
+        N = fingerprint.size
+        fp_value = fingerprint.value
+        fp_data = []
+        for p in combinations_with_replacement(self._symbols, 2):
+            fp_data.append(fp_value[p] if p in fp_value else -np.ones(N))
+        return list(chain.from_iterable(fp_data))
+
+    def extend(self, systems):
+        '''
+        :param systems: N * (ID + structure with enthalpy)
+        '''
+
+        if len(systems) <= self.DIMENSIONALITY:  # not enough points to build GCH
+            for ID, system in systems:
+                self._df.loc[len(self._df)] = ID, None, None, 0.0, 0.0
+            return
+
+        from sklearn.decomposition import PCA
+        pca = PCA(n_components=self.DIMENSIONALITY-1)
+        transformed = pca.fit_transform(np.array([self._flatten_fp(x.fingerprint) for _, x in systems]))
+
+        for (_, system), coord in zip(systems, transformed):
+            system.principal_component = coord
+            composition = self.config.numBlocks(system.composition)
+            system.enthalpy_per_block = system.enthalpy/np.sum(composition)
+
+        super().extend(systems)
