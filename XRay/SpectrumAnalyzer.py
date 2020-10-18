099d0e39a59011f28f4604f15baa993538e4d7b2
Michele Galasso <m.galasso@yandex.com>
2020-10-18 20:41:19 +0300

    implemented optimization of the lattice factor

--- XRay/SpectrumAnalyzer.py
+++ XRay/SpectrumAnalyzer.py
@@ -9,6 +9,9 @@ Class which implements the fitness function for comparing X-ray spectra
 
 import numpy as np
 
+from copy import deepcopy
+from scipy.optimize import minimize
+from pymatgen.core.lattice import Lattice
 from pymatgen.core.structure import Structure
 from pymatgen.analysis.diffraction.xrd import XRDCalculator
 
@@ -43,6 +46,8 @@ class SpectrumAnalyzer(object):
         :type exp_intensities: list[float]
         :param exp_intensities: intensities of the experimental spectrum.
         """
+        self.k = None
+
         self.spectrum_starts = spectrum_starts
         self.spectrum_ends = spectrum_ends
         self.wavelength = wavelength
@@ -59,20 +64,82 @@ class SpectrumAnalyzer(object):
         :rtype: float
         :return: a fitness denoting how much the theoretical spectrum differs from the experiment.
         """
-        # compute agreement
-        amplitude = self.spectrum_ends - self.spectrum_starts
-        calculator = XRDCalculator(wavelength=self.wavelength)
-
-        tmp = Structure(lattice=system.cell, species=system.get_chemical_symbols(), coords=system.scaled_coordinates)
-        string = tmp.to(fmt='cif', symprec=0.2)
-        structure = Structure.from_str(string, fmt='cif')
-
         # pure hydrogen gets low agreement
         if list(system.composition.keys()) == ['H']:
             return 100.0
 
+        # symmetrize the candidate structure
+        tmp = Structure(lattice=system.cell, species=system.get_chemical_symbols(), coords=system.scaled_coordinates)
+        string = tmp.to(fmt='cif', symprec=0.2)
+        structure = Structure.from_str(string, fmt='cif')
+
+        # initialize the lattice factor k
+        params = np.array([1.0])
+
+        # compute the lattice factor which minimize fitness
+        result = minimize(self.fitness_function, params, (structure,), method='Powell', bounds=((0.98, 1.02),),
+                          options={'ftol': 1.0e-4})
+
+        if not result.success:
+            raise RuntimeError('Scipy minimize could not calculate the agreement with experimental X-ray data.')
+
+        # extract the lattice factor
+        self.k = result.x[0]
+
+        # return fitness
+        return result.fun
+
+    @staticmethod
+    def parse(filename: str):
+        """
+        It parses a file containing information about the experimental spectrum and
+        it returns a dictionary containing the parameters for class initialization.
+
+        :type filename: str
+        :param filename: filename.
+        :rtype: dict
+        :return: kwargs parsed from the file.
+        """
+        dct = {}
+        with open(filename, 'r') as f:
+            current_line = f.readline()
+            while current_line != '':
+                values = current_line.split()
+                if values[0] == 'start':
+                    dct['spectrum_starts'] = float(values[1])
+                elif values[0] == 'end':
+                    dct['spectrum_ends'] = float(values[1])
+                elif values[0] == 'wavelength':
+                    dct['wavelength'] = float(values[1])
+                elif values[0] == 'match_tol':
+                    dct['match_tol'] = float(values[1])
+                elif values[0] == 'peaks':
+                    dct['exp_angles'] = [float(values[1])]
+                    dct['exp_intensities'] = [float(values[2])]
+                    current_line = f.readline()
+                    while current_line != '':
+                        values = current_line.split()
+                        dct['exp_angles'].append(float(values[0]))
+                        dct['exp_intensities'].append(float(values[1]))
+                        current_line = f.readline()
+                current_line = f.readline()
+        return dct
+
+    def fitness_function(self, params, structure):
+        # initialize fitness
         fitness = 0
-        pattern = calculator.get_pattern(structure, two_theta_range=(self.spectrum_starts, self.spectrum_ends))
+
+        # initialize XRDCalculator
+        calculator = XRDCalculator(wavelength=self.wavelength)
+
+        # get amplitude of the experimental spectrum
+        amplitude = self.spectrum_ends - self.spectrum_starts
+
+        # apply lattice correction and get pattern
+        k = params[0]
+        candidate = deepcopy(structure)
+        candidate.lattice = Lattice(np.diag([k, k, k]) @ candidate.lattice.matrix)
+        pattern = calculator.get_pattern(candidate, two_theta_range=(self.spectrum_starts, self.spectrum_ends))
 
         # ignore very small theoretical peaks (th_intensity < 1)
         indices = np.nonzero(pattern.y > 1)
@@ -112,42 +179,6 @@ class SpectrumAnalyzer(object):
         return fitness
 
     @staticmethod
-    def parse(filename: str):
-        """
-        It parses a file containing information about the experimental spectrum and
-        it returns a dictionary containing the parameters for class initialization.
-
-        :type filename: str
-        :param filename: filename.
-        :rtype: dict
-        :return: kwargs parsed from the file.
-        """
-        dct = {}
-        with open(filename, 'r') as f:
-            current_line = f.readline()
-            while current_line != '':
-                values = current_line.split()
-                if values[0] == 'start':
-                    dct['spectrum_starts'] = float(values[1])
-                elif values[0] == 'end':
-                    dct['spectrum_ends'] = float(values[1])
-                elif values[0] == 'wavelength':
-                    dct['wavelength'] = float(values[1])
-                elif values[0] == 'match_tol':
-                    dct['match_tol'] = float(values[1])
-                elif values[0] == 'peaks':
-                    dct['exp_angles'] = [float(values[1])]
-                    dct['exp_intensities'] = [float(values[2])]
-                    current_line = f.readline()
-                    while current_line != '':
-                        values = current_line.split()
-                        dct['exp_angles'].append(float(values[0]))
-                        dct['exp_intensities'].append(float(values[1]))
-                        current_line = f.readline()
-                current_line = f.readline()
-        return dct
-
-    @staticmethod
     def choose_factor(intensity: float, choices=FACTORS):
         """
         Simple auxiliary function which selects a factor based on the value of intensity.
@@ -160,13 +191,11 @@ class SpectrumAnalyzer(object):
         :return: importance factor chosen according to the intensity.
         """
         if intensity > 90:
-            factor = choices[0]
-        elif 50 < intensity <= 90:
-            factor = choices[1]
-        elif 10 < intensity <= 50:
-            factor = choices[2]
-        elif 1 < intensity <= 10:
-            factor = choices[3]
-        else:
-            factor = choices[4]
-        return factor
+            return choices[0]
+        if 50 < intensity <= 90:
+            return choices[1]
+        if 10 < intensity <= 50:
+            return choices[2]
+        if 1 < intensity <= 10:
+            return choices[3]
+        return choices[4]
