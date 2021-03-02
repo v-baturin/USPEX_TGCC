307170ece6f864f1fe027cf41bd320bcac79ca0a
Michele Galasso <m.galasso@yandex.com>
2021-03-02 10:19:06 +0300

    Now experimental reflections with zero intensity are not read from the hkl file

--- XRay/SpectrumAnalyzer.py
+++ XRay/SpectrumAnalyzer.py
@@ -160,11 +160,11 @@ class SpectrumAnalyzer(object):
                     if values == ['0', '0', '0', '0.00', '0.00']:
                         break
 
-                    hkl = (int(values[0]), int(values[1]), int(values[2]))
                     i_hkl = float(values[3])
-                    sigma_hkl = float(values[4])
-
-                    dct['exp_reflections'].append([i_hkl, hkl, sigma_hkl])
+                    if i_hkl > 0:
+                        hkl = (int(values[0]), int(values[1]), int(values[2]))
+                        sigma_hkl = float(values[4])
+                        dct['exp_reflections'].append([i_hkl, hkl, sigma_hkl])
             else:
                 current_line = f.readline()
                 while current_line != '':
