ec22060e35c55abca1690af3bb0a45cd90eac755
Pavel Bushlanov <paulbush@mail.ru>
2021-05-25 20:31:44 +0300

    fix bug in CellUtility.initFromCellParameters, degrees to radians

--- Atomistic/CellUtility.py
+++ Atomistic/CellUtility.py
@@ -200,6 +200,7 @@ class Cell:
 
     @staticmethod
     def initFromCellParameters(a, b, c, alpha, beta, gamma, pbc):
+        alpha, beta, gamma = np.pi / 180 * np.asarray((alpha, beta, gamma), dtype=float)
         va = np.array([a, 0, 0])
         vb = np.array([b*np.cos(gamma), b*np.sin(gamma), 0])
         cx = np.cos(beta)
