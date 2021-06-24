fb5e581d032aa4a163bd199dc7aacd35b045282e
Pavel Bushlanov <paulbush@mail.ru>
2021-06-24 22:35:47 +0300

    fix Output_test

--- USPEX/IO/unittests/Output_Test.py
+++ USPEX/IO/unittests/Output_Test.py
@@ -78,6 +78,8 @@ class Output_Test(unittest.TestCase):
                     optimizer.pool.allSystems[ID] = system
                     system['isBad'] = False
                 optimizer.best = set(targetState[0])
+                optimizer.fitness = optimizer.Fitness.calculate(optimizer.pool.uniqueSystems, optimizer.optType,
+                                                                optimizer.target.utilities)
                 optimizers.append(optimizer)
             with open(os.path.join(TESTPATH, f"output_data/population{gen}"), "r") as f:
                 populations.append([systems[ID][-1] for ID in json.load(f)])
