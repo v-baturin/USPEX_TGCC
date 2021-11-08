36a00a8cedacb1f1909ea14a1c8a2018aca1ebb3
Pavel Bushlanov <paulbush@mail.ru>
2021-11-08 19:59:31 +0000

    Resolve "Scale molecular structures in mutations if necessary"

--- USPEX/Atomistic/Operators/Softmodemutation.py
+++ USPEX/Atomistic/Operators/Softmodemutation.py
@@ -16,6 +16,7 @@ class Softmodemutation:
         self.ionDistances = utilities.ionDistances
         self.world = utilities.world
         self.conditions = utilities.conditions
+        self.cellUtility = utilities.cellUtility
         self.degree= degree
         self.knownSystems = {}
 
@@ -23,50 +24,51 @@ class Softmodemutation:
         ID = system['ID']
         molecules = system['molecules']
         cell = system['cell']
-        structure, disassembler = self.simpleMoleculeUtility.structureType.assemble(molecules, cell)
-        degree = self.degree if self.degree else np.mean([el.covalent_radius for el in structure.getAtomTypes()]) * 3
-        if ID in self.knownSystems:
-            frequencies, eigenVectors = self.knownSystems[ID]
-        else:
-            frequencies, eigenVectors = calcSoftModes(structure)
-            self.knownSystems[ID] = (frequencies, eigenVectors)
-        while len(frequencies) > 0:
-            freq = frequencies.pop(0)
-            eigenVector = eigenVectors.pop(0)
-            if freq < _MIN_VALID_FREQUENCY:
-                continue
-            displacements = eigenVector.reshape((len(structure),3))
-            displacements *= degree/np.max(np.linalg.norm(displacements, axis = 1))
-            molecules1 = []
-            molecules2 = []
-            for (transformation, _), molecule in zip(disassembler.decomposeDisplacements(displacements, structure),
-                                                                               molecules):
+        if self.cellUtility.isGoodCell(cell):
+            structure, disassembler = self.simpleMoleculeUtility.structureType.assemble(molecules, cell)
+            degree = self.degree if self.degree else np.mean([el.covalent_radius for el in structure.getAtomTypes()]) * 3
+            if ID in self.knownSystems:
+                frequencies, eigenVectors = self.knownSystems[ID]
+            else:
+                frequencies, eigenVectors = calcSoftModes(structure)
+                self.knownSystems[ID] = (frequencies, eigenVectors)
+            while len(frequencies) > 0:
+                freq = frequencies.pop(0)
+                eigenVector = eigenVectors.pop(0)
+                if freq < _MIN_VALID_FREQUENCY:
+                    continue
+                displacements = eigenVector.reshape((len(structure),3))
+                displacements *= degree/np.max(np.linalg.norm(displacements, axis = 1))
+                molecules1 = []
+                molecules2 = []
+                for (transformation, _), molecule in zip(disassembler.decomposeDisplacements(displacements, structure),
+                                                                                   molecules):
 
-                offset = Transformation.fromRotVector([0.,0.,0.,], molecule.getCenterOfMassCartesianCoordinates())
-                molecule = (-offset).transform(molecule)
-                molecule1 = transformation.transform(molecule)
-                molecule2 = (-transformation).transform(molecule)
-                molecule1 = offset.transform(molecule1)
-                molecule2 = offset.transform(molecule2)
-                molecules1.append(molecule1)
-                molecules2.append(molecule2)
+                    offset = Transformation.fromRotVector([0.,0.,0.,], molecule.getCenterOfMassCartesianCoordinates())
+                    molecule = (-offset).transform(molecule)
+                    molecule1 = transformation.transform(molecule)
+                    molecule2 = (-transformation).transform(molecule)
+                    molecule1 = offset.transform(molecule1)
+                    molecule2 = offset.transform(molecule2)
+                    molecules1.append(molecule1)
+                    molecules2.append(molecule2)
 
-            offsprings = ()
-            atomSymbols, atomDistances = self.simpleMoleculeUtility.getMinDistances(molecules1, cell)
-            minDistMatrix = self.ionDistances.getDistances(atomSymbols, self.conditions.externalPressure)
-            if np.all(atomDistances >= minDistMatrix):
-                system = {'molecules': molecules1, 'cell': cell}
-                self.world.putEnvironment(system)
-                self.conditions.putConditions(system)
-                offsprings += (system,)
-            atomSymbols, atomDistances = self.simpleMoleculeUtility.getMinDistances(molecules2, cell)
-            minDistMatrix = self.ionDistances.getDistances(atomSymbols, self.conditions.externalPressure)
-            if np.all(atomDistances >= minDistMatrix):
-                system = {'molecules': molecules2, 'cell': cell}
-                self.world.putEnvironment(system)
-                self.conditions.putConditions(system)
-                offsprings += (system,)
-            if offsprings:
-                return offsprings
+                offsprings = ()
+                atomSymbols, atomDistances = self.simpleMoleculeUtility.getMinDistances(molecules1, cell)
+                minDistMatrix = self.ionDistances.getDistances(atomSymbols, self.conditions.externalPressure)
+                if np.all(atomDistances >= minDistMatrix):
+                    system = {'molecules': molecules1, 'cell': cell}
+                    self.world.putEnvironment(system)
+                    self.conditions.putConditions(system)
+                    offsprings += (system,)
+                atomSymbols, atomDistances = self.simpleMoleculeUtility.getMinDistances(molecules2, cell)
+                minDistMatrix = self.ionDistances.getDistances(atomSymbols, self.conditions.externalPressure)
+                if np.all(atomDistances >= minDistMatrix):
+                    system = {'molecules': molecules2, 'cell': cell}
+                    self.world.putEnvironment(system)
+                    self.conditions.putConditions(system)
+                    offsprings += (system,)
+                if offsprings:
+                    return offsprings
 
         raise RuntimeError("Softmutation failed.")
--- USPEX/Atomistic/Operators/Transmutation.py
+++ USPEX/Atomistic/Operators/Transmutation.py
@@ -11,6 +11,7 @@ class Transmutation:
         self.world = utilities.world
         self.ionDistances = utilities.ionDistances
         self.conditions = utilities.conditions
+        self.cellUtility = utilities.cellUtility
         if self.simpleMoleculeUtility.isTrueMolecular:
             raise RuntimeError("Transmutation does not currently work in molecular regime.")
         self.specificTrans = []
@@ -20,34 +21,35 @@ class Transmutation:
     def __call__(self, system, *args, **kwargs):
         molecules = system['molecules']
         cell = system['cell']
-        symbolsIn = self.simpleMoleculeUtility.moleculeTypes(system)
-        symbolsOut = self.compositionSpace.symbols
+        if self.cellUtility.isGoodCell(cell):
+            symbolsIn = self.simpleMoleculeUtility.moleculeTypes(system)
+            symbolsOut = self.compositionSpace.symbols
 
-        trans = np.array([(i,sOut) for i, sIn in enumerate(symbolsIn) for sOut in symbolsOut if sIn != sOut],
-                         dtype = [('index', int),('symbol', 'U10')])
+            trans = np.array([(i,sOut) for i, sIn in enumerate(symbolsIn) for sOut in symbolsOut if sIn != sOut],
+                             dtype = [('index', int),('symbol', 'U10')])
 
-        for _ in range(self.transAttempts):
-            numberOfTrans = np.random.randint(1, self.howManyTrans + 1)
-            permutation = np.random.choice(trans, numberOfTrans)
-            transCoordinates = {}
-            excluded = []
-            for i, s in permutation:
-                excluded.append(i)
-                if s in transCoordinates:
-                    transCoordinates[s].append([molecules[i].getCenterOfMassCartesianCoordinates()])
-                else:
-                    transCoordinates[s] = [[molecules[i].getCenterOfMassCartesianCoordinates()]]
+            for _ in range(self.transAttempts):
+                numberOfTrans = np.random.randint(1, self.howManyTrans + 1)
+                permutation = np.random.choice(trans, numberOfTrans)
+                transCoordinates = {}
+                excluded = []
+                for i, s in permutation:
+                    excluded.append(i)
+                    if s in transCoordinates:
+                        transCoordinates[s].append([molecules[i].getCenterOfMassCartesianCoordinates()])
+                    else:
+                        transCoordinates[s] = [[molecules[i].getCenterOfMassCartesianCoordinates()]]
 
-            offspringMolecules = [molecule for i, molecule in enumerate(molecules) if i not in excluded]
-            offspringMolecules.extend(self.simpleMoleculeUtility.populateStructure(cell, transCoordinates, None))
+                offspringMolecules = [molecule for i, molecule in enumerate(molecules) if i not in excluded]
+                offspringMolecules.extend(self.simpleMoleculeUtility.populateStructure(cell, transCoordinates, None))
 
-            offspring = {'molecules': offspringMolecules, 'cell': cell}
-            atomSymbols, atomDistances = self.simpleMoleculeUtility.getMinDistances(offspringMolecules, cell)
-            minDistMatrix = self.ionDistances.getDistances(atomSymbols, self.conditions.externalPressure)
-            composition = self.simpleMoleculeUtility.composition(offspring)
-            if np.all(atomDistances >= minDistMatrix) and self.compositionSpace.isGoodComposition(composition):
-                self.world.putEnvironment(offspring)
-                self.conditions.putConditions(offspring)
-                return (offspring,)
+                offspring = {'molecules': offspringMolecules, 'cell': cell}
+                atomSymbols, atomDistances = self.simpleMoleculeUtility.getMinDistances(offspringMolecules, cell)
+                minDistMatrix = self.ionDistances.getDistances(atomSymbols, self.conditions.externalPressure)
+                composition = self.simpleMoleculeUtility.composition(offspring)
+                if np.all(atomDistances >= minDistMatrix) and self.compositionSpace.isGoodComposition(composition):
+                    self.world.putEnvironment(offspring)
+                    self.conditions.putConditions(offspring)
+                    return (offspring,)
 
         raise RuntimeError("Transmutation failed.")
