3a8cccd81cba652b8acde88f90bb4a61944920c4
System User <amazitov@miptstorage.cluster>
2022-03-11 20:52:51 +0300

    Initial refactoring

--- USPEX/Calculators/LAMMPS_Interface.py
+++ USPEX/Calculators/LAMMPS_Interface.py
@@ -3,7 +3,7 @@
 @author:     Arslan Mazitov
 @copyright:  2019 Oganov's Lab. All rights reserved.
 @contact:    arslan.mazitov@phystech.edu
-@date        10 Jan 2019
+@date        11 March 2022
 @brief       Class for calculator of LAMMPS
 '''
 
@@ -15,15 +15,16 @@ import shutil
 
 import numpy as np
 from ase.io import read
+from ase import Atoms
 from typing import List
 from os.path import join as pj
-from scipy.spatial.transform.rotation import Rotation
-
-from ..Atomistic.Transformation import Transformation
 from .Common.SHELL_Interface import SHELL_Interface
 logger = logging.getLogger(__name__)
 
 
+AVAILABLE_TARGET_OBEJECTS = ['default', 'environment']
+REQUIRED_THERMO_STYLE_PROPERTIES = ['enthalpy', 'etotal', 'ke', 'pe', 'temp', 
+                                    'pxx', 'pyy', 'pzz', 'pxy', 'pxz', 'pyz']
 
 class LAMMPS_Interface(SHELL_Interface):
     '''
@@ -31,39 +32,46 @@ class LAMMPS_Interface(SHELL_Interface):
     Local running
     '''
 
-    sleepTime = 10
-    _required_thermo_style_properties = ['ke', 'pe', 'etotal', 'enthalpy', 'pxx', 'pyy','pzz', 'pxy', 'pyz', 'pxz']
-    _required_dump_properties = ['vx', 'vy', 'vz', 'fx', 'fy', 'fz']
-    failedSystems = []
-
+    # working output files
     inputFile = 'lammps.in'
     outputFile = 'lammps.out'
-    errorFile = 'lammps.err'
-    logFile = 'log.lammps'
+    
+    log_file = 'log.lammps'
+    data_file = 'STRUC'
+    dump_file = 'lammps.dump'
+    
 
+
+    _DEFAULT_SLEEP_TIME = 30
     structureType = None
     atomType = None
     cellType = None
     atomicDisassemblerType = None
 
-    def __init__(self, tag: str, lammps_in: str = None, libs:List[str] = None, vacuumSize=10, **kwargs):
+    def __init__(self, tag: str, lammps_in: str, libs: List[str], specorder: List[str],
+                 vacuumSize: float = 10.0, targetObject: str = 'default', **kwargs):
         '''
 
         :param params: dictionary with parameters:
                 * lammps_in: (str) path to lammps.in-file.
-                * libs: (list) list of paths to interatomic potentials.
+                * libs: (list) list of paths to interatomic potentials and associated files.
         '''
 
         super().__init__(**kwargs)
-        if lammps_in is None:
-            lammps_in = pj(os.getcwd(), f'Specific/lammps.in_{tag}')
 
-        assert os.path.exists(lammps_in)
+        self.lammps_in = lammps_in
+        self.specorder = specorder
+        assert os.path.exists(self.lammps_in)
 
-        self.lammps_in  = lammps_in
-        self.libs = libs if libs else []
+        if libs is not None:
+            self.libs = libs
+
+        assert all([os.path.exists(lib) for lib in libs])
+
+        self.failedSystems = []
         self.vacuumSize = vacuumSize
-        logger.debug('LAMMPS calculator created.')
+        self.targetObject = targetObject.lower()
+        assert self.targetObject in AVAILABLE_TARGET_OBEJECTS
 
     def prepareLocalCalculation(self, system, calcFolder : str):
         '''
@@ -72,99 +80,76 @@ class LAMMPS_Interface(SHELL_Interface):
         '''
         structure, disassembler = self.structureType.assemble(**system)
         system['disassembler'] = disassembler
-        system['atomTypes'] = structure.getAtomTypes()
 
-        # coordinates = structure.getCartesianCoordinates()
-        # cell = structure.getRectifiedCell().getEnvelopeCell(coordinates, self.vacuumSize)
-        # coordinates = cell.center(coordinates)
-        # structure = self.structureType(structure.getAtomTypes(), coordinates, cell)
-
+        coordinates = structure.getCartesianCoordinates()
+        cell = structure.getRectifiedCell().getEnvelopeCell(coordinates, self.vacuumSize)
+        coordinates = cell.center(coordinates)
+        
         if not os.path.exists(calcFolder):
             os.makedirs(calcFolder)
-        # shutil.copy2(self.lammps_in, pj(calcFolder, self.inputFile))
         
         with open(self.lammps_in, 'r') as f:
             content = f.readlines()
-
-        if not np.any(['variable f string lammps.dump' in item for item in content]):
-            content.insert(0, 'variable f string lammps.dump\n')
-
-        try:
-            index = np.flatnonzero(['read_data' in item for item in content])[0]
-            content[index] = 'read_data STRUC\n'
-        except IndexError:
-            index = np.flatnonzero(['box' in item for item in content])[0]
-            content.insert(index+1, 'read_data STRUC\n')
-
+        
+        
+        # Step 1. We check if our lammps data file will be read properly 
+        # with read_data command when calculation is started
+        
+        read_data_occurence = ['read_data' in line for line in content]
+        pair_style_occurence = ['pair_style' in line for line in content]
+        read_data_content = f'read_data {self.data_file}\n'
+        if any(read_data_occurence):
+            index = np.where(read_data_occurence)[0][0]
+            content[index] = read_data_content
+        elif any(pair_style_occurence):
+            index = np.where(pair_style_occurence)[0][0]
+            content.insert(index-1, read_data_content)
             
-        # index = np.flatnonzero(['pair_coeff' in item for item in content])[0]
-        # composition = list(system.composition.keys())
-        # key = self.libs[0].split('/')[-1]
-        # key_idx = content[index].split().index(key)+1
-        # pair_coeff = content[index].split()[:key_idx] + composition
-        # # if pair_coeff[3] != self.libs[0]:
-        # #     pair_coeff[3] = self.libs[0]
-        # content[index] = " ".join(pair_coeff)
-        # content[index] += '\n'
-            
-        # index = np.flatnonzero(['thermo_style' in item for item in content])[0]
-        # thermo_style = content[index].splitlines()[0]
-        # for prop in self._required_thermo_style_properties:
-        #     if prop not in thermo_style:
-        #         thermo_style += " " + prop
-        # thermo_style += '\n'
-        # content[index] = thermo_style
-
-        try:
-            index = np.flatnonzero(['run ' in item for item in content])[-1]
-            nsteps = int(content[index].split()[-1])
-        except:
-            nsteps = 1000
-
-        dump = f'dump d1 all custom {nsteps} ${{f}} id type x y z vx vy vz fx fy fz\n'
-        try:
-            index = np.flatnonzero(['dump ' in item for item in content])[-1]
-            # dump = content[index].splitlines()[0]
-            # dump_name = dump.split()[5]
-            # if dump_name == 'lammps.dump' or dump_name == '${f}':
-            #     pass
-            # else:
-            #     dump = content[index].split()
-            #     dump[5] = 'lammps.dump'
-            #     dump = ' '.join(dump)
-            #
-            # for prop in self._required_dump_properties:
-            #     if prop not in dump:
-            #         dump += " " + prop
-            #
-            # dump += '\n'
-            content[index] = dump
-        except:
-            index = np.flatnonzero(['run' in item for item in content])[-1]
-            content.insert(index, dump)
-
-        for i in reversed(np.flatnonzero(['dump_modify' in item for item in content])):
-            del content[i]
-
+        # Step 2. We check if all required properties are specified with thermo_style
+        # command for further consistent output data reading 
+        
+        thermo_style_occurence = ['thermo_style' in line for line in content]
+        thermo_style_content = 'thermo_style custom step ' + ' '.join(REQUIRED_THERMO_STYLE_PROPERTIES) + '\n'
+        if any(thermo_style_occurence):
+            index = np.where(read_data_occurence)[0][0]
+            content[index] = thermo_style_content
+        else:
+            index = np.where(pair_style_occurence)[0][0]
+            content.insert(index+1, thermo_style_content)
+        # Step 3. We check if lammps.dump file will be written properly 
+        # with dump command after calculation is finished
+        
+        lammps_uspex_dump = f'dump lammps_uspex_dump all custom 1 lammps.dump id type x y z vx vy vz fx fy fz\n'
+        content.append(lammps_uspex_dump)
+        content.append('run 0\n')
+    
+        # Step 4. We write all the input files to our calcFolder
         
         with open(pj(calcFolder, self.inputFile), 'w') as f:
             f.writelines(content)
-             
-        write_data(structure, pj(calcFolder, 'STRUC'))
+        
+        if self.targetObject == 'default':
+            atoms = Atoms([el.short_name for el in structure.getAtomTypes()], coordinates, cell = cell.getCellVectors())
+            atoms.write(pj(calcFolder, self.data_file), format='lammps-data', specorder=self.specorder)
+        elif self.targetObject == 'environment':
+            environment = system['environment'].getStructure()
+            atoms = Atoms([el.short_name for el in environment.getAtomTypes()], environment.getCartesianCoordinates(), cell = cell.getCellVectors())
+            atoms.write(pj(calcFolder, self.data_file), format='lammps-data',  specorder=self.specorder)
+        
         for lib in self.libs:
-            if isinstance(lib,str) and os.path.exists(lib):
-                shutil.copy(lib,calcFolder)
-        logger.debug('LAMMPS calculator prepared calculation.')
+            shutil.copy2(lib, calcFolder)
+                
 
     def isConverged(self, calcFolder : str):
         lammps_completed = False
         tolerance_achieved = False
         if os.path.exists(pj(calcFolder, self.outputFile)):
             output = pj(calcFolder, self.outputFile)
-        elif os.path.exists(pj(calcFolder, self.logFile)):
-            output = pj(calcFolder, self.logFile)
+        elif os.path.exists(pj(calcFolder, self.log_file)):
+            output = pj(calcFolder, self.log_file)
         else:
             return False
+        
         with open(output, 'r') as f:
             content = f.readlines()
 
@@ -181,49 +166,41 @@ class LAMMPS_Interface(SHELL_Interface):
             if 'Breaking threshold exceeded' in line:
                 lammps_completed = True
                 tolerance_achieved = True
-            if 'No selection data provided with MLIP' in line:
-                lammps_completed = True
-                tolerance_achieved = True
-                struct = read(pj(calcFolder, 'STRUC'), format='lammps-data', style='atomic')
-                write_cfg(pj(calcFolder, 'sampled.cfg'), struct)
-
         
         if not tolerance_achieved:
             logger.error('LAMMPS minimization tolerance criteria is not achieved.')
-            shutil.copy(output,  pj(calcFolder, 'ERROR'+self.outputFile))
+            shutil.copy(output,  pj(calcFolder, 'ERROR-'+self.outputFile))
             self.failedSystems.append(calcFolder)
-        return lammps_completed and tolerance_achieved
+        return lammps_completed and tolerance_achieved        
 
     def readOutput(self, system, calcFolder : str):
         cell = system['cell']
         disassembler = system['disassembler']
         del system['disassembler']
-        atomTypes = system['atomTypes']
-        del system['atomTypes']
-
-        from ase.io.lammpsrun import read_lammps_dump
-        atoms = read_lammps_dump(pj(calcFolder, 'lammps.dump'))
-        # TODO appears to be buggy
-        # cdisp = []
-        # with open(pj(calcFolder, 'lammps.dump'), 'r') as g:
-        #     glines = g.readlines()
-        # for line in glines[5:8]:
-        #     cdisp.append(-float(line.split()[0]))
-        # atoms.translate(cdisp)
-
-        positions = atoms.get_positions()
-        cell = self.cellType(atoms.get_cell().array, cell.getPBC()).getEnvelopeCell(positions, 0)
-        positions = cell.center(positions)
-        structure = self.structureType(atomTypes, positions, cell=cell)
-        system.update(disassembler.disassemble(structure))
 
         properties = self.readProperties(calcFolder)
-        system['enthalpy'] = properties['TotEng']
-        # system['stress'] = properties['pressureTensor']
-        system['energy']= properties['TotEng']
+        
+        if self.targetObject == 'default':
+            atoms = read(pj(calcFolder, self.dump_file), format='lammps-dump-text')
+            positions = atoms.get_positions()
+            numbers = atoms.get_atomic_numbers()
+            symbols = [self.specorder[i-1] for i in numbers]
+            atomTypes = np.array([self.atomType(symbol) for symbol in symbols], dtype=self.atomType)
+            cell = self.cellType(atoms.get_cell().array, cell.getPBC()).getEnvelopeCell(positions, 0)
+            positions = cell.center(positions)
+            system.update(disassembler.disassemble(self.structureType(atomTypes, positions, cell=cell)))
+            system['enthalpy'] = properties['Enthalpy']
+            system['energy'] = properties['TotEng']
+            system['stressTensor'] = properties['StressTensor']
+        elif self.targetObject == 'environment':
+            system['environmentEnthalpy'] = properties['Enthalpy']
 
     def readProperties(self, calcFolder: str):
-        with open(pj(calcFolder, self.outputFile), 'r') as f:
+        if os.path.exists(pj(calcFolder, self.outputFile)):
+            output = pj(calcFolder, self.outputFile)
+        elif os.path.exists(pj(calcFolder, self.log_file)):
+            output = pj(calcFolder, self.log_file)
+        with open(output, 'r') as f:
             content = f.readlines()
         
         for i, line in enumerate(content):
@@ -238,15 +215,14 @@ class LAMMPS_Interface(SHELL_Interface):
                 properties[properties_list[j]] = int(item)
             else:
                 properties[properties_list[j]] = float(item)
-        # pressureTensor = np.zeros((3,3))
-        # pressureTensor[0][0], pressureTensor[1][1], pressureTensor[2][2] = properties['Pxx'], properties['Pyy'], properties['Pzz']
-        # pressureTensor[0][1] = properties['Pxy']
-        # pressureTensor[1][0] = properties['Pxy']
-        # pressureTensor[0][2] = properties['Pxz']
-        # pressureTensor[2][0] = properties['Pxz']
-        # pressureTensor[1][2] = properties['Pyz']
-        # pressureTensor[2][1] = properties['Pyz']
-        # properties['pressureTensor'] = pressureTensor
+        stressTensor = np.zeros((3,3))
+        stressTensor[0][0] = properties['Pxx']
+        stressTensor[1][1] = properties['Pyy']
+        stressTensor[2][2] = properties['Pzz']
+        stressTensor[0][1] = stressTensor[1][0] = properties['Pxy']
+        stressTensor[0][2] = stressTensor[2][0] = properties['Pxz']
+        stressTensor[1][2] = stressTensor[2][1] = properties['Pyz']
+        properties['StressTensor'] = stressTensor
         return properties
 
     @classmethod
@@ -256,96 +232,3 @@ class LAMMPS_Interface(SHELL_Interface):
         cls.cellType = cellType
         cls.atomicDisassemblerType = atomicDisassemblerType
 
-def write_cfg(fname, item):
-    from ase.calculators.calculator import Calculator
-    with open(fname, 'w') as f:
-        forces = None
-        stresses = None
-        energy = None
-        positions = item.get_positions()
-        if hasattr(item, '_calc'):
-            if isinstance(item._calc, Calculator):
-                results = item._calc.results
-                if 'forces' in results.keys():
-                    forces = results['forces']
-                if 'stress' in results.keys():
-                    stresses = results['stress']
-                if 'energy' in results.keys():
-                    energy = results['free_energy']
-
-        f.write("BEGIN_CFG\n")
-        f.write(" Size\n")
-        f.write(f" {len(item)}\n")
-        f.write(" Supercell\n")
-        for vector in item.get_cell():
-            f.write(f"\t{vector[0]:.6f}\t{vector[1]:.6f}\t{vector[2]:.6f}\n")
-        atom_header = " AtomData:  id type       cartes_x      cartes_y      cartes_z           "
-        if forces is not None:
-            atom_header += "fx          fy          fz"
-        atom_header += "\n"
-        f.write(atom_header)
-        for i, atom in enumerate(item):
-            p = positions[i]
-            atom_data = f"  \t  {i+1}\t{item.numbers[i]-1}\t{p[0]:.6f}\t\t{p[1]:.6f}\t\t{p[2]:.6f}\t\t"
-            if forces is not None:
-                fs = forces[i]
-                atom_data += f"{fs[0]:.6f}\t{fs[1]:.6f}\t{fs[2]:.6f}"
-            atom_data += "\n"
-            f.write(atom_data)
-        if energy is not None:
-            f.write(" Energy\n")
-            f.write(f"\t{energy:.12f}\n")
-        if stresses is not None:
-            f.write(" PlusStress:  xx          yy          zz          yz          xz          xy\n")
-            f.write(f"\t{stresses[0]:.5f}\t{stresses[1]:.5f}\t{stresses[2]:.5f}\t{stresses[3]:.5f}\t{stresses[4]:.5f}\t{stresses[5]:.5f}\n")
-        if energy is not None or forces is not None or stresses is not None:
-            f.write("Feature   EFS_by    VASP\n")
-        if hasattr(item, 'ID'):
-            f.write(f"Feature   AS_index {item.ID}\n")
-        f.write("END_CFG\n")
-        f.write("\n")
-    
-def write_data(structure, filename, comment=None):
-
-    from ase.data import atomic_masses, atomic_numbers
-    a = structure.getCell().getCellVectors()[0]
-    rotation, _ = Rotation.align_vectors(a.reshape(1,3), np.array([[1.0, 0.0, 0.0]]))
-    transformation = Transformation.fromRotVector(rotation.as_rotvec(), [0.,0.,0.])
-    structure = transformation.transform(structure)
-    cell = structure.getCell().getCellVectors()
-    # specorder = system.specorder
-    specorder, atom_ids = np.unique([el.short_name for el in structure.getAtomTypes()], return_inverse=True)
-    positions = structure.getCartesianCoordinates()
-    num_unique_types = len(specorder)
-    # atom_ids = [specorder.index(item)+1 for item in system.get_chemical_symbols()]
-    masses = [atomic_masses[atomic_numbers[item]] for item in specorder]
-
-    with open(filename, 'w') as f:
-        if comment is None:
-            comment = 'LAMMPS_Interface data file'
-        f.write(comment.strip() + '\n\n')
-
-        f.write('{} atoms\n'.format(len(structure)))
-        f.write('{} atom types\n'.format(num_unique_types))
-        f.write('\n')
-        f.write('{0:16.8e} {1:16.8e} xlo xhi\n'.format(0.0, cell[0, 0]))
-        if cell[1,1] < 0:
-            f.write('{0:16.8e} {1:16.8e} ylo yhi\n'.format(cell[1, 1], 0.0))
-        else:
-            f.write('{0:16.8e} {1:16.8e} ylo yhi\n'.format(0.0, cell[1, 1]))
-
-        if cell[2, 2] < 0:
-            f.write('{0:16.8e} {1:16.8e} zlo zhi\n'.format(cell[2, 2], 0.0))
-        else:
-            f.write('{0:16.8e} {1:16.8e} zlo zhi\n'.format(0.0, cell[2, 2]))
-        f.write('{0:16.8e} {1:16.8e} {2:16.8e} xy xz yz\n'
-                ''.format(cell[1, 0], cell[2, 0], cell[2, 1]))
-
-        f.write('\nMasses\n\n')
-        for id, mass in enumerate(masses):
-            f.write('{} {}\n'.format(id+1, mass))
-        f.write('\nAtoms # atomic\n\n')
-        for i, (id, pos) in enumerate(
-                zip(atom_ids+1, positions)):
-            f.write('{} {}{:16.8e} {:16.8e} {:16.8e}\n'
-                    .format(i + 1, id, pos[0], pos[1], pos[2]))
