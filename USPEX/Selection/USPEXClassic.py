b9560db80c7f6befbba31244ba2b8dcf05aafd7e
Pavel Bushlanov <paulbush@mail.ru>
2022-08-29 15:42:33 +0300

    handle antiseeds proterties, fix output test

--- USPEX/Selection/USPEXClassic.py
+++ USPEX/Selection/USPEXClassic.py
@@ -83,7 +83,8 @@ FunctionFolder/USPEX/3**/EA_3**.m
 class USPEXClassic(object):
 
     def __init__(self, pool, target, fingerprintUtility, optType, popSize : int, fractions : Dict[str, tuple],
-                 initialPopSize=None, bestFrac:float=0.7, howManyDiverse=None, diversityTolerance = 0.5, debug = False, **kwargs):
+                 initialPopSize=None, bestFrac:float=0.7, howManyDiverse=None, diversityTolerance = 0.5, debug = False,
+                 antiseeds: dict = None, **kwargs):
         """
         :param target: reference to configuration space object
         :param params: dictionary contains following parameters:
@@ -91,7 +92,7 @@ class USPEXClassic(object):
         """
         self.pool = pool
         self.target = target
-        self.target.utilities.antiseeds = Antiseeds()
+        self.target.utilities.antiseeds = Antiseeds(antiseeds)
         self.fingerprintUtility = fingerprintUtility
         self.optType = optType
         self.fractions = fractions
