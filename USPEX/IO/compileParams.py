f2efb4804278c66624371c31273a984d577e2712
Pavel Bushlanov <paulbush@mail.ru>
2022-08-29 15:30:09 +0300

    construct elementalSymbols in compileParams

--- USPEX/IO/compileParams.py
+++ USPEX/IO/compileParams.py
@@ -14,11 +14,15 @@ def compileParams(main: dict) -> dict:
         target = optimizer['target']
         symbols = target['compositionSpace']['symbols']
         molecules = {}
+        elementalSymbols = set()
         for i, symbol in enumerate(symbols):
             if isinstance(symbol, dict):
                 molDct = read_molecule(symbol['filename'])
                 molecules[symbol['name']] = molDct
                 symbols[i] = symbol['name']
+                elementalSymbols |= set(molDct['symbols'])
+            else:
+                elementalSymbols.add(symbol)
             if molecules:
                 target['simpleMoleculeUtility'] = {'molecules': molecules}
         if 'ionDistances' not in target:
@@ -41,6 +45,6 @@ def compileParams(main: dict) -> dict:
         if 'radialDistributionUtility' not in target:
             target['radialDistributionUtility'] = {}
         if 'symbols' not in target['radialDistributionUtility']:
-            target['radialDistributionUtility']['symbols'] = target['compositionSpace']['symbols']
+            target['radialDistributionUtility']['symbols'] = list(elementalSymbols)
 
     return main
