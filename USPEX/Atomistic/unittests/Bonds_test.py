9bc448ba5ce1601f21d3ef83e258b886ada02644
Vladimir Baturin <vsbat@yandex.ru>
2022-10-31 23:16:31 +0100

    add tests for isConnected

--- USPEX/Atomistic/unittests/Bonds_test.py
+++ USPEX/Atomistic/unittests/Bonds_test.py
@@ -809,4 +809,37 @@ class test_BondHardness(unittest.TestCase):
 
         self.assertTrue('pop from empty' in str(context.exception))
 
+    def test_CheckConnectivity_cluster(self):
+        tmp = read_vasp(pj(CURRENT_DIR, 'P11H3_badstruct.POSCARS'))
+        cell = Cell(tmp.get_cell().array, (0, 0, 0))
+        system = AtomicStructure(symbolsToElements(tmp.get_chemical_symbols()),
+                                 cell.fractionalToCartesian(tmp.get_scaled_positions()),
+                                 cell=cell)
+        bonds = Bonds()
+        self.assertTrue(bonds.isConnected(system, checkConnectivityCutoffFactor=2))
+        self.assertFalse(bonds.isConnected(system, checkConnectivityCutoffFactor=1.5))
+
+    def test_CheckConnectivity_graphite(self):
+        tmp = read_vasp(pj(CURRENT_DIR, 'graphite2.POSCAR'))
+        cell3d = Cell(tmp.get_cell().array, (1, 1, 1))
+        system = AtomicStructure(symbolsToElements(tmp.get_chemical_symbols()),
+                                 cell3d.fractionalToCartesian(tmp.get_scaled_positions()),
+                                 cell=cell3d)
+        bonds = Bonds()
+        self.assertFalse(bonds.isConnected(system, checkConnectivityCutoffFactor=2))
+        self.assertTrue(bonds.isConnected(system, checkConnectivityCutoffFactor=2.6))
+
+        tmp = read_vasp(pj(CURRENT_DIR, 'graphite_1layer.vasp'))
+        cell3d = Cell(tmp.get_cell().array, (1, 1, 1))
+        system = AtomicStructure(symbolsToElements(tmp.get_chemical_symbols()),
+                                 cell3d.fractionalToCartesian(tmp.get_scaled_positions()),
+                                 cell=cell3d)
+        self.assertFalse(bonds.isConnected(system, checkConnectivityCutoffFactor=1.5))
+        cell2d = Cell(tmp.get_cell().array, (1, 1, 0))
+        system = AtomicStructure(symbolsToElements(tmp.get_chemical_symbols()),
+                                 cell3d.fractionalToCartesian(tmp.get_scaled_positions()),
+                                 cell=cell2d)
+        self.assertTrue(bonds.isConnected(system, checkConnectivityCutoffFactor=1.5))
+
+
 
