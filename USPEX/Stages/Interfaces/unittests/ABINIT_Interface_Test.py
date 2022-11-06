5b290bf2f2676b9a36f9d48e3795a94f59891a8a
Pavel Bushlanov <paulbush@mail.ru>
2022-11-06 22:11:09 +0300

    reenable and actualize ABINIT interface

--- USPEX/Stages/Interfaces/unittests/ABINIT_Interface_Test.py
+++ USPEX/Stages/Interfaces/unittests/ABINIT_Interface_Test.py
@@ -15,9 +15,8 @@ import filecmp
 from os.path import join as pj
 
 
-# from USPEX.Atomistic.RadialDistributionUtility import RadialDistributionUtility
-# from ..ABINIT_Interface import ABINIT_Interface
-# from USPEX.components import AtomisticRepresentation
+from USPEX.Atomistic.RadialDistributionUtility import RadialDistributionUtility
+from USPEX.components import AtomisticRepresentation, ABINIT_Interface
 
 
 HOMEPATH = os.path.dirname(os.path.abspath(__file__))
@@ -26,34 +25,35 @@ GATHEREDPATH = os.path.join(HOMEPATH, 'abinitGatheredData')
 WORKPATH = os.path.join(HOMEPATH, 'Eu2H18_abinit')
 
 
-# class ABINIT_Interface_Test(unittest.TestCase):
-#     """
-#     Checking correct parsing properties
-#     """
-#
-#     def test_life(self):
-#         abinit = ABINIT_Interface(tag='0', in_file=pj(SPECIFICPATH, 'abinit.in_1'), kresol=0.13,
-#                                   pp_files=[pj(SPECIFICPATH, 'H.psp8'), pj(SPECIFICPATH, 'Eu.psp8')])
-#         radialDistributionUtility = RadialDistributionUtility(symbols=['Eu', 'H'])
-#
-#
-#         for ID in range(10):
-#             system = AtomisticRepresentation.readAtomicStructure(pj(GATHEREDPATH, f'input/system{ID}.vasp'))
-#             system['assembled_cell'] = system['cell']
-#             system['ID'] = ID
-#             system['externalPressure'] = 130.0
-#             os.mkdir(WORKPATH)
-#             abinit.prepareLocalCalculation(system, WORKPATH)
-#             folder = pj(GATHEREDPATH, 'input', f"CalcFold{system['ID']}")
-#             dcmp = filecmp.dircmp(folder, WORKPATH)
-#             match = not dcmp.diff_files
-#             for common_dir in dcmp.common_dirs:
-#                 match = match and not dcmp.subdirs[common_dir].diff_files
-#             shutil.rmtree(WORKPATH)
-#             self.assertTrue(match)
-#             folder = pj(GATHEREDPATH, 'output')
-#             shutil.copytree(pj(folder, f"CalcFold{system['ID']}"), WORKPATH)
-#             abinit.readOutput(system, WORKPATH)
-#             shutil.rmtree(WORKPATH)
-#             systemRef = AtomisticRepresentation.readAtomicStructure(pj(folder, f"system{system['ID']}.vasp"))
-#             self.assertTrue(radialDistributionUtility.equal(system, systemRef))
+class ABINIT_Interface_Test(unittest.TestCase):
+    """
+    Checking correct parsing properties
+    """
+
+    def test_life(self):
+        abinit = ABINIT_Interface(tag='0', in_file=pj(SPECIFICPATH, 'abinit.in_1'), kresol=0.13,
+                                  pp_files=[pj(SPECIFICPATH, 'H.psp8'), pj(SPECIFICPATH, 'Eu.psp8')],
+                                  perturbate=False)
+        radialDistributionUtility = RadialDistributionUtility(symbols=['Eu', 'H'])
+
+
+        for ID in range(10):
+            system = AtomisticRepresentation.readAtomicStructure(pj(GATHEREDPATH, f'input/system{ID}.vasp'))
+            system['ID'] = ID
+            system['externalPressure'] = 130.0
+            system['tmp_0'] = {}
+            os.mkdir(WORKPATH)
+            abinit.prepareLocalCalculation(system, WORKPATH)
+            folder = pj(GATHEREDPATH, 'input', f"CalcFold{system['ID']}")
+            dcmp = filecmp.dircmp(folder, WORKPATH)
+            match = not dcmp.diff_files
+            for common_dir in dcmp.common_dirs:
+                match = match and not dcmp.subdirs[common_dir].diff_files
+            shutil.rmtree(WORKPATH)
+            self.assertTrue(match)
+            folder = pj(GATHEREDPATH, 'output')
+            shutil.copytree(pj(folder, f"CalcFold{system['ID']}"), WORKPATH)
+            abinit.readOutput(system, WORKPATH)
+            shutil.rmtree(WORKPATH)
+            systemRef = AtomisticRepresentation.readAtomicStructure(pj(folder, f"system{system['ID']}.vasp"))
+            self.assertTrue(radialDistributionUtility.equal(system, systemRef))
