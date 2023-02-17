fc70f8610e4841b869720532187a15fcb06299ea
Pavel Bushlanov <paulbush@mail.ru>
2023-02-17 16:01:17 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
-from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
-
-
-class bdist_wheel(_bdist_wheel):
-    def finalize_options(self):
-        super().finalize_options()
-        self.root_is_pure = False
-
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+    class bdist_wheel(_bdist_wheel):
+        def finalize_options(self):
+            super().finalize_options()
+            self.root_is_pure = False
+except ImportError:
+    bdist_wheel = None
 
 setup(
     cmdclass={'bdist_wheel': bdist_wheel},
