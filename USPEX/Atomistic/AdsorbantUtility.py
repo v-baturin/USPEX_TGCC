bf06aa3b998659624d64f13edb1b6a2e68509177
Vladimir Baturin <vsbat@yandex.ru>
2023-04-03 14:25:48 +0200

    introduce _calcRadius for adsorbants

--- USPEX/Atomistic/AdsorbantUtility.py
+++ USPEX/Atomistic/AdsorbantUtility.py
@@ -5,9 +5,13 @@ class Adsorbant:
         self.structure = structure
         self.mountpoint = mountpoint
         self.orientation = orientation
+        self.radius = self._calcRadius()
+
+    def _calcRadius(self):
+        pass
 
 
 
 class AdsorbantUtility(object):
     def __init__(self, adsorbants):
-        self.adsorbants = adsorbants
+        self.adsorbants = {key: Adsorbant(**adsorbant) for key, adsorbant in adsorbants.items()}
