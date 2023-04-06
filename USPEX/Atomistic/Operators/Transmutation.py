0a21790b92629a6b8efb8239227cc15936110373
Pavel Bushlanov <paulbush@mail.ru>
2023-04-06 15:40:15 +0300

    fix cartesian to fractional in transmutation

--- USPEX/Atomistic/Operators/Transmutation.py
+++ USPEX/Atomistic/Operators/Transmutation.py
@@ -37,7 +37,7 @@ class Transmutation:
                 excluded = []
                 for i, s in permutation:
                     excluded.append(i)
-                    position = molecules[i].getCenterOfMassCartesianCoordinates()
+                    position = molecules[i].getCenterOfMassFractionalCoordinates()
                     operation[0:3, 3] = position
                     if s in operations:
                         operations[s].append([[np.copy(operation)]])
