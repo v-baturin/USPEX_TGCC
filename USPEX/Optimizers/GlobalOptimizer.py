899c32ad8f2da4eec330cdfd3cdd7e13479dbd5c
Artem <samtsevichartem@gmail.com>
2023-04-25 18:42:44 +0200

    Prefinal version. V2.0.1

--- USPEX/Optimizers/GlobalOptimizer.py
+++ USPEX/Optimizers/GlobalOptimizer.py
@@ -58,7 +58,7 @@ class GlobalOptimizer(object):
         :type seeds: type
         :param seeds: type of Seeds operator.
         """
-        assert name not in cls.knownTargetTypes
+        assert name not in cls.knownTargetTypes, f'{name} is not registered as known Target'
         cls.knownTargetTypes[name] = TargetType(utilities=utilities, hybridizations=hybridizations,
                                                 mutations=mutations, creations=creations, seeds=seeds)
 
