91987a4de0406f410a8975129e35abcd440ca8b9
Dmitry Rybkovskiy <rybkovskiyd@gmail.com>
2023-05-05 15:06:38 +0300

    .gen format is used for geometry. Works for periodic and nonperiodic systems. Fixed atoms implemented.

--- USPEX/Stages/Interfaces/XTB_Interface.py
+++ USPEX/Stages/Interfaces/XTB_Interface.py
@@ -7,8 +7,10 @@ USPEX.Stages.XTB_Interface
 import logging
 import os
 from os.path import join as pj
+import numpy as np
 
-from ase.io import read
+from ase import Atoms
+from ase.io.gen import read_gen, write_gen
 
 logger = logging.getLogger(__name__)
 HARTREE_TO_EV = 27.211386245988 #https://physics.nist.gov/cgi-bin/cuu/Value?hrev
@@ -26,10 +28,10 @@ class XTB_Interface:
     cellType = None
 
     inputFile, outputFile, errorFile = 'xtb.inp', 'output', 'error'
-    geometry_file = 'uspex.xyz'
+    geometry_file = 'uspex.gen'
     specific_file = 'xtb.inp_'
 
-    out_geometry_file = 'xtbopt.xyz'
+    out_geometry_file = 'xtbopt.gen'
 
     @classmethod
     def registerTypes(cls, structureType, atomType, cellType):
@@ -51,18 +53,37 @@ class XTB_Interface:
         self.targetProperties = targetProperties if targetProperties is not None else ['structure', 'enthalpy']
 
     def prepareLocalCalculation(self, system, calcFolder : str):
-        structure = system['structure']
 
+        structure = system['structure']
         cell = structure.getCell()
         system['pbc'] = cell.getPBC()
+        symbols = np.asarray([el.short_name for el in structure.getAtomTypes()])
+        coordinates = structure.getCartesianCoordinates()
+        cell_vectors = cell.getCellVectors()
+        ase_struct = Atoms(symbols, positions=coordinates, cell=cell_vectors, pbc=system['pbc'])
+        write_gen(pj(calcFolder, self.geometry_file), ase_struct)
+
+        content_to_write = ''
+        fixedIndices = system['disassembler'].envIndices[
+            system['environment'].getFixedIndices()] if 'environment' in system else None
+        if fixedIndices is not None:
+            content_to_write += '$fix\n'
+            content_to_write += 'atoms: '
+            onebased_fixedIndices = fixedIndices + 1
+            indices = np.where(np.diff(onebased_fixedIndices) != 1)[0] + 1
+            groups = np.split(onebased_fixedIndices, indices)
+            for i, group in enumerate(groups):
+                if len(group) == 1:
+                    content_to_write += str(group[0])
+                else:
+                    content_to_write += f"{group[0]}-{group[-1]}"
+                if i < len(groups) - 1:
+                    content_to_write += ','
+            content_to_write += '\n$end\n'
+        total_content = self.xtb_input + '\n' + content_to_write + '\n'
 
         with open(pj(calcFolder, self.inputFile), 'wt') as dest:
-            dest.write(self.xtb_input)
-
-        with open(pj(calcFolder, self.geometry_file), 'wt') as fp:
-            fp.write('{}\n\n'.format(len(structure.getAtomTypes())))
-            for i, (symbol, coord) in enumerate(zip(structure.getAtomTypes(), structure.getCartesianCoordinates())):
-                fp.write('{0:2s}  {1:15.8f} {2:15.8f} {3:15.8f} \n'.format(symbol.short_name, *coord))
+            dest.write(total_content)
 
         return ''
 
@@ -80,31 +101,50 @@ class XTB_Interface:
 
     def readOutput(self, system, calcFolder: str):
 
-        new_structure, EnergyHa = self.readStructureAndEnergyHa(calcFolder, system.pop('pbc'))
-
         results = {}
         if 'structure' in self.targetProperties:
-            results['structure'] = new_structure
+            results['structure'] = self.readStructure(calcFolder, system.pop('pbc'))
         if 'energy' in self.targetProperties:
-            results['energy'] = EnergyHa * HARTREE_TO_EV
+            results['energy'] = self.readEnergyHa(calcFolder) * HARTREE_TO_EV
         if 'enthalpy' in self.targetProperties:
-            results['enthalpy'] = EnergyHa * HARTREE_TO_EV
+            results['enthalpy'] = self.readEnergyHa(calcFolder) * HARTREE_TO_EV
 
         return results
 
-    def readStructureAndEnergyHa(self, calcFolder: str, pbc):
+    def readStructure(self, calcFolder: str, pbc):
 
-        new_lattice = []
-        ase_struct = read(pj(calcFolder, self.out_geometry_file), index='-1')
+        LOCAL_VACUUM = 4.0
+
+        ase_struct = read_gen(pj(calcFolder, self.out_geometry_file))
         atomTypes = []
+        new_lattice = []
         for i in ase_struct.get_chemical_symbols():
             atomTypes.append(self.atomType(i))
         positions = ase_struct.get_positions()
+        tmp_lattice = ase_struct.cell[:].copy()
+        for i, vec in enumerate(tmp_lattice):
+            if pbc[i]:
+                new_lattice.append([float(x) for x in vec])
         cell = self.cellType.initFromCellVectors(pbc, new_lattice)
+#        if sum(pbc) == 0:
+#            xsize = np.amax(ase_struct.get_positions()[:, 0]) - np.amin(ase_struct.get_positions()[:, 0])
+#            ysize = np.amax(ase_struct.get_positions()[:, 1]) - np.amin(ase_struct.get_positions()[:, 1])
+#            zsize = np.amax(ase_struct.get_positions()[:, 2]) - np.amin(ase_struct.get_positions()[:, 2])
+#            new_lattice = np.array([[xsize + LOCAL_VACUUM, 0.0, 0.0], [0.0, ysize + LOCAL_VACUUM, 0.0],\
+#                                    [0.0, 0.0, zsize + LOCAL_VACUUM]])
+#        else:
+#            new_lattice = ase_struct.cell[:].copy()
+#        cell = self.cellType(new_lattice, pbc)
         new_structure = self.structureType(atomTypes, positions, cell=cell)
 
-        with open(pj(calcFolder, self.out_geometry_file), 'rt') as f:
-            content = f.readlines()
-            EnergyHa = float(content[1].split()[1])
+        return new_structure
 
-        return new_structure, EnergyHa
+    def readEnergyHa(self, calcFolder: str):
+
+        with open(pj(calcFolder, self.outputFile), 'rt') as f:
+            content = f.readlines()
+            for i in content:
+                if 'TOTAL ENERGY' in i:
+                    EnergyHa = float(i.split()[3])
+
+        return EnergyHa
