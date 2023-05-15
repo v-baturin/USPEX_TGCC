3a4f7eeeff0376d246333c87927187c91a0fd974
Vladimir Baturin <vsbat@yandex.ru>
2023-05-15 19:35:00 +0200

    use pathlib pt2

--- USPEX/Atomistic/unittests/EnvironmentUtility_Test.py
+++ USPEX/Atomistic/unittests/EnvironmentUtility_Test.py
@@ -1,6 +1,5 @@
 import unittest
-import os
-from os.path import join as pj
+from pathlib import Path
 import json
 import numpy as np
 from itertools import combinations_with_replacement
@@ -13,10 +12,10 @@ class EnvironmentUtility_TestNanoparticleCore(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-        cls.TEST_FILES_DIR = os.path.join(cls.CURRENT_DIR, 'core_adsorbant_files')
-        cls.data_NDI = json2dict(pj(cls.TEST_FILES_DIR, 'data_NDI.json'))
-        cls.data_Alpha = json2dict(pj(cls.TEST_FILES_DIR, 'data_Alpha.json'))
+        cls.CURRENT_DIR = Path(__file__).parent
+        cls.TEST_FILES_DIR = cls.CURRENT_DIR/'core_adsorbant_files'
+        cls.data_NDI = json2dict(cls.TEST_FILES_DIR/'data_NDI.json')
+        cls.data_Alpha = json2dict(cls.TEST_FILES_DIR/'data_Alpha.json')
         cls.NDI_core = cls.compile_core_envutility(cls, cls.data_NDI)
         cls.Alpha_core = cls.compile_core_envutility(cls, cls.data_Alpha)
         cls.moleculesNDI, cls.adsorbantsNDI = cls.compile_adsorbants(cls, cls.data_NDI)
@@ -27,7 +26,7 @@ class EnvironmentUtility_TestNanoparticleCore(unittest.TestCase):
         molecules = dict()
         for ads in data_core_adsorbant['adsorbants']:
             adsName = ads['name']
-            structure = AtomisticRepresentation.readXYZ(pj(self.TEST_FILES_DIR, ads['filename']))
+            structure = AtomisticRepresentation.readXYZ(self.TEST_FILES_DIR/ads['filename'])
             molecules[adsName] = structure
             adsSites = []
             for site in ads['sites']:
@@ -41,7 +40,7 @@ class EnvironmentUtility_TestNanoparticleCore(unittest.TestCase):
 
     def compile_core_envutility(self, data_core_adsorbant):
         for env_descr in data_core_adsorbant["environments"]:
-            env_descr['structure'] = AtomisticRepresentation.readXYZ(pj(self.TEST_FILES_DIR, env_descr['filename']))
+            env_descr['structure'] = AtomisticRepresentation.readXYZ(self.TEST_FILES_DIR/env_descr['filename'])
         return EnvironmentUtility(data_core_adsorbant["environments"])
 
     def test_dock_NDI(self):
@@ -54,7 +53,7 @@ class EnvironmentUtility_TestNanoparticleCore(unittest.TestCase):
         structure, disassembler = AtomicDisassembler.assemble(molecules=[new_ads_struct],
                                                               cell=Cell.initFromCellVectors((0,0,0)),
                                                               environment=assembler.assemble([new_ads_struct]))
-        AtomisticRepresentation.writeXYZ(pj(self.TEST_FILES_DIR, 'outNDI.xyz'), structure)
+        AtomisticRepresentation.writeXYZ(self.TEST_FILES_DIR/'outNDI.xyz', structure)
 
     def test_dock_Alpha(self):
         adsName = 'phenyl'
@@ -69,7 +68,7 @@ class EnvironmentUtility_TestNanoparticleCore(unittest.TestCase):
         structure, disassembler = AtomicDisassembler.assemble(molecules=[new_ads_struct],
                                                               cell=Cell.initFromCellVectors((0, 0, 0)),
                                                               environment=assembler.assemble([new_ads_struct]))
-        AtomisticRepresentation.writeXYZ(pj(self.TEST_FILES_DIR, 'outAlpha.xyz'), structure)
+        AtomisticRepresentation.writeXYZ(self.TEST_FILES_DIR/'outAlpha.xyz', structure)
 
 def json2dict(fname):
     with open(fname, 'r') as f:
--- USPEX/IO/unittests/readXYZs_test.py
+++ USPEX/IO/unittests/readXYZs_test.py
@@ -1,16 +1,16 @@
 import unittest
 import os
-from os.path import join as pj
+from pathlib import Path
 
 from ...components import AtomisticRepresentation
 
 
-HOMEPATH = os.path.dirname(os.path.abspath(__file__))
+HOMEPATH = Path(__file__)
 
 
 class ReadXYZs_Test(unittest.TestCase):
     def test_read_xyzs(self):
-        structures = AtomisticRepresentation.readXYZs(pj(HOMEPATH, 'several_structures.xyz'))
+        structures = AtomisticRepresentation.readXYZs(HOMEPATH/'several_structures.xyz')
         self.assertEqual(len(structures), 7)
         for structure in structures:
             self.assertEqual(len(structure), 40)
