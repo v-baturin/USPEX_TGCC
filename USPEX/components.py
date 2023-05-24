be253ebb5c1f50533fadfd1364ad99c996655de7
Vladimir Baturin <vsbat@yandex.ru>
2023-05-24 18:15:57 +0200

    excluded mentions of alphashape

--- USPEX/components.py
+++ USPEX/components.py
@@ -16,8 +16,8 @@ from .Atomistic.Environments.Substrate import Substrate
 Substrate.registerTypes(AtomisticRepresentation, AtomicStructure, Element, Cell, AtomicDisassembler)
 from .Atomistic.Environments.Bulk import Bulk
 Bulk.registerTypes(AtomisticRepresentation, AtomicStructure, Element, Cell, AtomicDisassembler)
-from .Atomistic.Environments.NanoparticleCore import NanoparticleCore
-NanoparticleCore.registerTypes(AtomisticRepresentation, AtomicStructure, Element, Cell, AtomicDisassembler)
+# from .Atomistic.Environments.NanoparticleCore import NanoparticleCore
+# NanoparticleCore.registerTypes(AtomisticRepresentation, AtomicStructure, Element, Cell, AtomicDisassembler)
 from .Atomistic.RadialDistributionUtility import RadialDistributionUtility
 RadialDistributionUtility.registerTypes(AtomicStructure, Element, Cell, AtomicDisassembler)
 from .Atomistic.CellUtility import CellUtility
@@ -28,13 +28,13 @@ from .Atomistic.JunctionUtility import JunctionUtility
 from .Atomistic.Conditions import Conditions
 from .Atomistic.BondUtility import BondUtility
 BondUtility.registerTypes(Element, AtomicDisassembler)
-from .Atomistic.ElasticML import ElasticML
-ElasticML.registerTypes(AtomicDisassembler)
+# from .Atomistic.ElasticML import ElasticML
+# ElasticML.registerTypes(AtomicDisassembler)
 from .Atomistic.Constraints import Constraints
-from .XRay.PowderSpectrumAnalyzer import PowderSpectrumAnalyzer
-PowderSpectrumAnalyzer.registerTypes(AtomicStructure, Element, Cell, AtomicDisassembler)
-from .XRay.SingleCrystalSpectrumAnalyzer import SingleCrystalSpectrumAnalyzer
-SingleCrystalSpectrumAnalyzer.registerTypes(AtomicStructure, Element, Cell, AtomicDisassembler)
+# from .XRay.PowderSpectrumAnalyzer import PowderSpectrumAnalyzer
+# PowderSpectrumAnalyzer.registerTypes(AtomicStructure, Element, Cell, AtomicDisassembler)
+# from .XRay.SingleCrystalSpectrumAnalyzer import SingleCrystalSpectrumAnalyzer
+# SingleCrystalSpectrumAnalyzer.registerTypes(AtomicStructure, Element, Cell, AtomicDisassembler)
 from .Atomistic.Operators.Heredity import Heredity
 from .Atomistic.Operators.RandTop import RandTop
 from .Atomistic.Operators.RandSym import RandSym
@@ -46,15 +46,16 @@ from .Atomistic.Operators.AddAtom import AddAtom
 from .Atomistic.Operators.RemoveAtom import RemoveAtom
 from .Atomistic.Operators.TeleportAtom import TeleportAtom
 from .Atomistic.Operators.Seeds import Seeds
-from .Atomistic.Operators.CoreAdsorbantRandomGenerator import CoreAdsorbantRandomGenerator
+# from .Atomistic.Operators.CoreAdsorbantRandomGenerator import CoreAdsorbantRandomGenerator
 Seeds.registerTypes(AtomisticRepresentation)
 GlobalOptimizer.registerTarget('Atomistic',
                       utilities=[CompositionSpace, RadialDistributionUtility, CellUtility, EnvironmentUtility,
-                                 SimpleMoleculeUtility, Conditions, BondUtility, Constraints, ElasticML,
-                                 PowderSpectrumAnalyzer, SingleCrystalSpectrumAnalyzer, JunctionUtility],
+                                 SimpleMoleculeUtility, Conditions, BondUtility, Constraints, JunctionUtility],
+                                 # ElasticML,
+                                 # PowderSpectrumAnalyzer, SingleCrystalSpectrumAnalyzer],
                       hybridizations=[Heredity],
                       mutations=[Softmodemutation, Permutation, Transmutation, AddAtom, RemoveAtom, TeleportAtom],
-                      creations=[RandTop, RandSym, RandSymPyXtal, CoreAdsorbantRandomGenerator],
+                      creations=[RandTop, RandSym, RandSymPyXtal], # CoreAdsorbantRandomGenerator],
                       seeds=Seeds)
 from .Stages.Executor import Executor
 from .Stages.Interfaces.ASEInterfaceAdapter import ASEInterfaceAdapter
@@ -65,29 +66,29 @@ Executor.registerInterface('abinit', ABINIT_Interface)
 from .Stages.Interfaces.GULP_Interface import GULP_Interface
 GULP_Interface.registerTypes(AtomicStructure, Element, Cell)
 Executor.registerInterface('gulp', GULP_Interface)
-from .Stages.Interfaces.LAMMPS_Interface import LAMMPS_Interface
-LAMMPS_Interface.registerTypes(AtomisticRepresentation, ASEInterfaceAdapter.LAMMPS)
-Executor.registerInterface('lammps', LAMMPS_Interface)
-from .Stages.Interfaces.MLIP_Interface import MLIP_Interface
-MLIP_Interface.registerTypes(AtomisticRepresentation, AtomicDisassembler)
-Executor.registerInterface('mlip', MLIP_Interface)
-from .Stages.Interfaces.PWmat_Interface import PWmat_Interface
-PWmat_Interface.registerTypes(AtomicStructure, Element, Cell)
-from .Stages.Interfaces.QE_Interface import QE_Interface
-QE_Interface.registerTypes(ASEInterfaceAdapter.QE)
-Executor.registerInterface('qe', QE_Interface)
+# from .Stages.Interfaces.LAMMPS_Interface import LAMMPS_Interface
+# LAMMPS_Interface.registerTypes(AtomisticRepresentation, ASEInterfaceAdapter.LAMMPS)
+# Executor.registerInterface('lammps', LAMMPS_Interface)
+# from .Stages.Interfaces.MLIP_Interface import MLIP_Interface
+# MLIP_Interface.registerTypes(AtomisticRepresentation, AtomicDisassembler)
+# Executor.registerInterface('mlip', MLIP_Interface)
+# from .Stages.Interfaces.PWmat_Interface import PWmat_Interface
+# PWmat_Interface.registerTypes(AtomicStructure, Element, Cell)
+# from .Stages.Interfaces.QE_Interface import QE_Interface
+# QE_Interface.registerTypes(ASEInterfaceAdapter.QE)
+# Executor.registerInterface('qe', QE_Interface)
 from .Stages.Interfaces.VASP_Interface import VASP_Interface
 VASP_Interface.registerTypes(ASEInterfaceAdapter.VASP)
 Executor.registerInterface('vasp', VASP_Interface)
 from .Stages.Interfaces.MOPAC_Interface import MOPAC_Interface
 MOPAC_Interface.registerTypes(AtomicStructure, Element, Cell)
 Executor.registerInterface('mopac', MOPAC_Interface)
-from .Stages.Interfaces.FHIaims_Interface import FHIaims_Interface
-FHIaims_Interface.registerTypes(AtomicStructure, Element, Cell)
-Executor.registerInterface('aims', FHIaims_Interface)
-from .Stages.Interfaces.CP2K_Interface import CP2K_Interface
-CP2K_Interface.registerTypes(AtomicStructure, Element, Cell)
-Executor.registerInterface('cp2k', CP2K_Interface)
+# from .Stages.Interfaces.FHIaims_Interface import FHIaims_Interface
+# FHIaims_Interface.registerTypes(AtomicStructure, Element, Cell)
+# Executor.registerInterface('aims', FHIaims_Interface)
+# from .Stages.Interfaces.CP2K_Interface import CP2K_Interface
+# CP2K_Interface.registerTypes(AtomicStructure, Element, Cell)
+# Executor.registerInterface('cp2k', CP2K_Interface)
 from .Stages.TaskManagers.BSUB import BSUB
 Executor.registerTaskManager('BSUB', BSUB)
 from .Stages.TaskManagers.QSUB import QSUB
@@ -103,11 +104,12 @@ External.setExecutorType(Executor)
 ModelOptimizer.registerModel(External)
 ModelOptimizer.registerTarget('Atomistic',
                       utilities=[CompositionSpace, RadialDistributionUtility, CellUtility, EnvironmentUtility,
-                                 SimpleMoleculeUtility, Conditions, BondUtility, Constraints, ElasticML,
-                                 PowderSpectrumAnalyzer, SingleCrystalSpectrumAnalyzer, JunctionUtility],
+                                 SimpleMoleculeUtility, Conditions, BondUtility, Constraints, JunctionUtility],
+                                 # ElasticML,
+                                 # PowderSpectrumAnalyzer, SingleCrystalSpectrumAnalyzer],
                       hybridizations=[Heredity],
                       mutations=[Softmodemutation, Permutation, Transmutation, AddAtom, RemoveAtom, TeleportAtom],
-                      creations=[RandTop, RandSym, RandSymPyXtal, CoreAdsorbantRandomGenerator],
+                      creations=[RandTop, RandSym, RandSymPyXtal], # CoreAdsorbantRandomGenerator],
                       seeds=Seeds)
 from .Stages.AtomisticStage import AtomisticStage
 AtomisticStage.registerTypes(Executor, AtomicDisassembler)
