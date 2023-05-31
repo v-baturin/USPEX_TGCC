a51c3da65b5743375287d14db069fc44e0cbca75
Pavel Bushlanov <paulbush@mail.ru>
2023-05-31 16:24:38 +0300

    do not subtract one from specorder index in readMLIPcfg

--- USPEX/IO/AtomisticRepresentation.py
+++ USPEX/IO/AtomisticRepresentation.py
@@ -233,7 +233,7 @@ class AtomisticRepresentation(object):
 
         cell = cls.cellType(lat, (1, 1, 1))
         if specorder is not None:
-            types = [specorder[n-1] for n in types]
+            types = [specorder[n] for n in types]
         return dict(
             structure=cls.structureType([cls.atomType(n) for n in types], pos, cell=cell),
             energy=energy,
@@ -268,7 +268,7 @@ class AtomisticRepresentation(object):
             f.write(atstr1)
         else:
             f.write(atstr2)
-        atomTypes =  [specorder.index(el.short_name) for el in structure.getAtomTypes()]
+        atomTypes = [specorder.index(el.short_name) for el in structure.getAtomTypes()]
         positions = structure.getCartesianCoordinates()
         for i in range(size):
             if forces is not None:
