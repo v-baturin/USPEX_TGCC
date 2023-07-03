2a79367183ddf123760edc56a2a743e57840c70f
Vladimir Baturin <vsbat@yandex.ru>
2023-07-03 22:57:10 +0200

    fix initialization of simpleMoleculeUtility

--- USPEX/IO/compileParams.py
+++ USPEX/IO/compileParams.py
@@ -40,8 +40,10 @@ def compileParams(main: dict) -> dict:
                 symbols[i] = symbol['name']
                 elementalSymbols |= set([x.short_name for x in structure.getAtomTypes()])
         target['junctionUtility'] = {'molSitesMapping': molSitesMapping}
+        if 'simpleMoleculeUtility' not in target:
+            target['simpleMoleculeUtility'] = {}
         if molecules:
-            target['simpleMoleculeUtility'] = {'molecules': molecules}
+            target['simpleMoleculeUtility']['molecules'] = molecules
         if 'selection' in optimizer:
             selection = optimizer['selection']
             if len(target['compositionSpace']['blocks']) > 1:
