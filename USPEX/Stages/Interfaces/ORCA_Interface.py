1afa2343af24b982d66509db3ff45a0586a2bc35
Dmitry Rybkovskiy <rybkovskiyd@gmail.com>
2023-08-07 18:49:26 +0300

    new handling of fixed indices

--- USPEX/Stages/Interfaces/ORCA_Interface.py
+++ USPEX/Stages/Interfaces/ORCA_Interface.py
@@ -79,7 +79,8 @@ class ORCA_Interface:
 
         orca_input_lines[xyz_line_index] = '* xyz  {}  {}'.format(charge, multiplicity)
 
-        fixedIndices = np.copy(system['disassembler'].allFixedIndices)
+        disassembler = system.getProperty('disassembler', prefix='atomistic', suffix='intermediate')
+        fixedIndices = disassembler.allFixedIndices
         for i, (symbol, coord) in enumerate(zip(structure.getAtomTypes(), structure.getCartesianCoordinates())):
             if i in fixedIndices:
                 orca_input_lines.append('{0:2s}  {1:15.8f}$ {2:15.8f}$ {3:15.8f}$'
