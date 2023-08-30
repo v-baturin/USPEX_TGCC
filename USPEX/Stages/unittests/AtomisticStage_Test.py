2f53d90ac5ee849ac0f0a2acb8c2176645b71394
Pavel Bushlanov <paulbush@mail.ru>
2023-08-30 18:52:48 +0300

    minor fix in AtomisticStage test

--- USPEX/Stages/unittests/AtomisticStage_Test.py
+++ USPEX/Stages/unittests/AtomisticStage_Test.py
@@ -69,14 +69,14 @@ class AtomisticStage_Test(unittest.TestCase):
         badWrappingFilePath = PATH_WITH_TESTS/'mol_wrapping_POSCARS.uspex'
         systemSource, systemSink = Atomistic.readAtomicStructures(badWrappingFilePath)
         system = PoolEntry(0, EntryFlavour(extensions=extensions, **systemSource))
-        system.system['0'] = EntryFlavour(extensions=extensions, **systemSink)
+        system.addFlavour('0', EntryFlavour(extensions=extensions, **systemSink))
         self.assertTrue(self.checkWrapped(system))
         atomisticStage.checkAndFixMolecules(system)
         self.assertFalse(self.checkWrapped(system))
         brokenMolFilePath = PATH_WITH_TESTS / 'mol_wrapping_POSCARS_brokenMol.uspex'
         systemSource, systemSink = Atomistic.readAtomicStructures(brokenMolFilePath)
         system = PoolEntry(0, EntryFlavour(extensions=extensions, **systemSource))
-        system.system['0'] = EntryFlavour(extensions=extensions, **systemSink)
+        system.addFlavour('0', EntryFlavour(extensions=extensions, **systemSink))
         atomisticStage.checkAndFixMolecules(system)
         self.assertTrue(system.getProperty('isBad', suffix='0'))
 
