3ec37bf0e42c67e87d79954f5df7b60ce3c802f5
Vladimir Baturin <vsbat@yandex.ru>
2023-11-23 20:02:27 +0100

    comment unnecessary components and  add TGCC registration

--- USPEX/components.py
+++ USPEX/components.py
@@ -16,8 +16,8 @@ from .Atomistic.Environments.Substrate import Substrate
 Substrate.registerTypes(Atomistic)
 from .Atomistic.Environments.Bulk import Bulk
 Bulk.registerTypes(Atomistic)
-from .Atomistic.Environments.NanoparticleCore import NanoparticleCore
-NanoparticleCore.registerTypes(Atomistic)
+# from .Atomistic.Environments.NanoparticleCore import NanoparticleCore
+# NanoparticleCore.registerTypes(Atomistic)
 from .Atomistic.RadialDistributionUtility import RadialDistributionUtility
 from .Atomistic.CellUtility import CellUtility
 from .Atomistic.SimpleMoleculeUtility import SimpleMoleculeUtility
@@ -25,9 +25,9 @@ SimpleMoleculeUtility.registerTypes(AtomicStructure, Element)
 from .Atomistic.JunctionUtility import JunctionUtility
 from .Atomistic.Conditions import Conditions
 from .Atomistic.BondUtility import BondUtility
-from .Atomistic.ElasticML import ElasticML
-from .XRay.PowderSpectrumAnalyzer import PowderSpectrumAnalyzer
-from .XRay.SingleCrystalSpectrumAnalyzer import SingleCrystalSpectrumAnalyzer
+# from .Atomistic.ElasticML import ElasticML
+# from .XRay.PowderSpectrumAnalyzer import PowderSpectrumAnalyzer
+# from .XRay.SingleCrystalSpectrumAnalyzer import SingleCrystalSpectrumAnalyzer
 from .Atomistic.Operators.Heredity import Heredity
 from .Atomistic.Operators.RandTop import RandTop
 from .Atomistic.Operators.RandSym import RandSym
@@ -39,7 +39,7 @@ from .Atomistic.Operators.AddAtom import AddAtom
 from .Atomistic.Operators.RemoveAtom import RemoveAtom
 from .Atomistic.Operators.TeleportAtom import TeleportAtom
 from .Atomistic.Operators.Seeds import Seeds
-from .Atomistic.Operators.CoreAdsorbantRandomGenerator import CoreAdsorbantRandomGenerator
+# from .Atomistic.Operators.CoreAdsorbantRandomGenerator import CoreAdsorbantRandomGenerator
 Seeds.registerTypes(AtomicStructureRepresentation)
 # --------------------------------------------- Optimizers ----------------------------------------------------------
 from .Optimizers.GlobalOptimizer import GlobalOptimizer
@@ -49,52 +49,58 @@ from .Selection.USPEXClassic import USPEXClassic
 GlobalOptimizer.registerSelection(USPEXClassic)
 GlobalOptimizer.registerTarget('Atomistic',
                                utilities=[Atomistic, CompositionSpace, RadialDistributionUtility, CellUtility,
-                                 EnvironmentUtility, SimpleMoleculeUtility, Conditions, BondUtility, ElasticML,
-                                 PowderSpectrumAnalyzer, SingleCrystalSpectrumAnalyzer, JunctionUtility],
+                                 EnvironmentUtility, SimpleMoleculeUtility, Conditions, BondUtility,
+                                          # ElasticML, PowderSpectrumAnalyzer, SingleCrystalSpectrumAnalyzer, JunctionUtility
+                                          ],
                                hybridizations=[Heredity],
                                mutations=[Softmodemutation, Permutation, Transmutation, AddAtom, RemoveAtom, TeleportAtom],
-                               creations=[RandTop, RandSym, RandSymPyXtal, CoreAdsorbantRandomGenerator],
+                               creations=[RandTop, RandSym, RandSymPyXtal,
+                                          # CoreAdsorbantRandomGenerator
+                                          ],
                                seeds=Seeds)
 from .Optimizers.ModelOptimizer import ModelOptimizer, External
 ModelOptimizer.registerModel(External)
 ModelOptimizer.registerTarget('Atomistic',
                       utilities=[Atomistic, CompositionSpace, RadialDistributionUtility, CellUtility,
-                                 EnvironmentUtility, SimpleMoleculeUtility, Conditions, BondUtility, ElasticML,
-                                 PowderSpectrumAnalyzer, SingleCrystalSpectrumAnalyzer, JunctionUtility],
+                                 EnvironmentUtility, SimpleMoleculeUtility, Conditions, BondUtility,
+                                 # ElasticML, PowderSpectrumAnalyzer, SingleCrystalSpectrumAnalyzer, JunctionUtility
+                                 ],
                       hybridizations=[Heredity],
                       mutations=[Softmodemutation, Permutation, Transmutation, AddAtom, RemoveAtom, TeleportAtom],
-                      creations=[RandTop, RandSym, RandSymPyXtal, CoreAdsorbantRandomGenerator],
+                      creations=[RandTop, RandSym, RandSymPyXtal,
+                                 # CoreAdsorbantRandomGenerator
+                                 ],
                       seeds=Seeds)
 from .Stages.Executor import Executor
 External.setExecutorType(Executor)
 # --------------------------------------------- Interfaces ----------------------------------------------------------
-from .Stages.Interfaces.ABINIT_Interface import ABINIT_Interface
-Executor.registerInterface('abinit', ABINIT_Interface)
-from .Stages.Interfaces.GULP_Interface import GULP_Interface
-Executor.registerInterface('gulp', GULP_Interface)
-from .Stages.Interfaces.LAMMPS_Interface import LAMMPS_Interface
-LAMMPS_Interface.registerTypes(AtomicStructureRepresentation)
-Executor.registerInterface('lammps', LAMMPS_Interface)
-from .Stages.Interfaces.MLIP_Interface import MLIP_Interface
-Executor.registerInterface('mlip', MLIP_Interface)
-from .Stages.Interfaces.QE_Interface import QE_Interface
-QE_Interface.registerTypes(AtomicStructureRepresentation)
-Executor.registerInterface('qe', QE_Interface)
+# from .Stages.Interfaces.ABINIT_Interface import ABINIT_Interface
+# Executor.registerInterface('abinit', ABINIT_Interface)
+# from .Stages.Interfaces.GULP_Interface import GULP_Interface
+# Executor.registerInterface('gulp', GULP_Interface)
+# from .Stages.Interfaces.LAMMPS_Interface import LAMMPS_Interface
+# LAMMPS_Interface.registerTypes(AtomicStructureRepresentation)
+# Executor.registerInterface('lammps', LAMMPS_Interface)
+# from .Stages.Interfaces.MLIP_Interface import MLIP_Interface
+# Executor.registerInterface('mlip', MLIP_Interface)
+# from .Stages.Interfaces.QE_Interface import QE_Interface
+# QE_Interface.registerTypes(AtomicStructureRepresentation)
+# Executor.registerInterface('qe', QE_Interface)
 from .Stages.Interfaces.VASP_Interface import VASP_Interface
 VASP_Interface.registerTypes(AtomicStructureRepresentation)
 Executor.registerInterface('vasp', VASP_Interface)
-from .Stages.Interfaces.MOPAC_Interface import MOPAC_Interface
-Executor.registerInterface('mopac', MOPAC_Interface)
-from .Stages.Interfaces.FHIaims_Interface import FHIaims_Interface
-Executor.registerInterface('aims', FHIaims_Interface)
-from .Stages.Interfaces.XTB_Interface import XTB_Interface
-XTB_Interface.registerTypes(AtomicStructureRepresentation)
-Executor.registerInterface('xtb', XTB_Interface)
-from .Stages.Interfaces.DFTBplus_Interface import DFTBplus_Interface
-DFTBplus_Interface.registerTypes(AtomicStructureRepresentation)
-Executor.registerInterface('dftb', DFTBplus_Interface)
-from .Stages.Interfaces.CP2K_Interface import CP2K_Interface
-Executor.registerInterface('cp2k', CP2K_Interface)
+# from .Stages.Interfaces.MOPAC_Interface import MOPAC_Interface
+# Executor.registerInterface('mopac', MOPAC_Interface)
+# from .Stages.Interfaces.FHIaims_Interface import FHIaims_Interface
+# Executor.registerInterface('aims', FHIaims_Interface)
+# from .Stages.Interfaces.XTB_Interface import XTB_Interface
+# XTB_Interface.registerTypes(AtomicStructureRepresentation)
+# Executor.registerInterface('xtb', XTB_Interface)
+# from .Stages.Interfaces.DFTBplus_Interface import DFTBplus_Interface
+# DFTBplus_Interface.registerTypes(AtomicStructureRepresentation)
+# Executor.registerInterface('dftb', DFTBplus_Interface)
+# from .Stages.Interfaces.CP2K_Interface import CP2K_Interface
+# Executor.registerInterface('cp2k', CP2K_Interface)
 # -------------------------------------------- Task Managers --------------------------------------------------------
 from .Stages.TaskManagers.BSUB import BSUB
 Executor.registerTaskManager('BSUB', BSUB)
@@ -104,6 +110,8 @@ from .Stages.TaskManagers.SBATCH import SBATCH
 Executor.registerTaskManager('SBATCH', SBATCH)
 from .Stages.TaskManagers.SHELL import SHELL
 Executor.registerTaskManager('SHELL', SHELL)
+from .Stages.TaskManagers.TGCC import TGCC
+Executor.registerTaskManager('TGCC', TGCC)
 from .Stages.AtomisticStage import AtomisticStage
 # ------------------------------------------------ Stages -----------------------------------------------------------
 AtomisticStage.registerTypes(Executor)
