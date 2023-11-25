935e854787d37c28f0f1beca391af3f2c0cf39e4
Vladimir Baturin <vsbat@yandex.ru>
2023-11-25 14:49:02 +0100

    first commit

--- USPEX/Stages/AtomisticStage.py
+++ USPEX/Stages/AtomisticStage.py
@@ -1,4 +1,5 @@
 import logging
+
 import numpy as np
 from itertools import product
 from ase.geometry import get_distances
@@ -18,15 +19,16 @@ class AtomisticStage:
         cls.executorType = executorType
 
     def __init__(self, tag, source=None, perturbate: bool = False, target=None, environmentStyle=None, vacuumSize=0,
-                 **kwargs):
+                 targetProperties=None, **kwargs):
         self.tag = tag
         self.source = source
         self.perturbate = perturbate
         self.target = target
         self.environmentStyle = environmentStyle
         self.vacuumSize = vacuumSize
+        self.targetProperties = targetProperties if targetProperties is not None else ['structure', 'enthalpy']
         self.kwargs = kwargs
-        self.executor = self.executorType(tag=tag, **kwargs)
+        self.executor = self.executorType(tag=tag, targetProperties=self.targetProperties, **kwargs)
 
     async def run(self, system: PoolEntry):
         if self.environmentStyle != 'noEnvironment':
@@ -41,6 +43,7 @@ class AtomisticStage:
         if self.perturbate:
             structure = structure.getPerturbatedStructure(disassembler.fixedIndices)
         intermediate = disassembler.disassemble(structure)
+        intermediate['.ID'] = system.ID
         intermediate['.vacuumSize'] = self.vacuumSize
         intermediate['.externalPressure'] = system.getProperty('externalPressure', suffix='origin')
         intermediate = system.flavourFactory(**intermediate)
@@ -55,12 +58,11 @@ class AtomisticStage:
         disassembler = intermediate.getProperty('disassembler', extension='atomistic')
         result.setProperty('disassembler', disassembler, extension='atomistic')
         self.systemCheckAndFix(result)
-        if 'targetProperties' in self.kwargs and 'enthalpy' in self.kwargs['targetProperties'] \
-                and '.enthalpy' not in system.getFlavour(self.tag):
+        if 'enthalpy' in self.targetProperties and '.enthalpy' not in result:
             structure = result.getProperty('structure', extension='atomistic')
             pressure = system.getProperty('externalPressure', suffix='origin')
             energy = result.getProperty('energy')
-            enthalpy = energy + structure.getVolume() * pressure * self.EV_PER_CUBIC_ANGSTREM_PER_GPA
+            enthalpy = energy + structure.getCell().getVolume() * pressure * self.EV_PER_CUBIC_ANGSTREM_PER_GPA
             result.setProperty('enthalpy', enthalpy)
         system.addFlavour(self.tag, result)
         self.checkAndFixMolecules(system)
@@ -112,6 +114,7 @@ class AtomisticStage:
                 else:
                     logger.info(f'system {system["ID"]}: broken molecule detected')
                     system.setProperty('isBad', True, suffix=self.tag)
+                    break
 
         for i, coords in correctorDict.items():
             badMol = moleculesSink[i]
@@ -154,5 +157,7 @@ class AtomisticStage:
         wrappingsOfB = coordB - allwrappings @ cellSink.getCellVectors()
         good_wrap_idx = np.where(np.abs(get_distances(wrappingsOfB, coordA)[1] - sourceDist) / sourceDist <
                                  self.target.utilities.simpleMoleculeUtility.integrityTol)[0]
-        if good_wrap_idx:
+        if len(good_wrap_idx) == 1:
             return wrappingsOfB[good_wrap_idx[0]]
+        elif len(good_wrap_idx) > 1:
+            logger.warning(f"atom with frac coords {cellSink.cartesianToFractional(coordB):.3f}: ambiguous dewrapping")
