d7517dabbc02fecbe6a1240ff52e8e8684494994
Pavel Bushlanov <paulbush@mail.ru>
2023-11-27 14:54:44 +0300

    clarify constraints violation messges

--- USPEX/Stages/AtomisticStage.py
+++ USPEX/Stages/AtomisticStage.py
@@ -57,7 +57,7 @@ class AtomisticStage:
             return
         disassembler = intermediate.getProperty('disassembler', extension='atomistic')
         result.setProperty('disassembler', disassembler, extension='atomistic')
-        self.systemCheckAndFix(result)
+        self.systemCheckAndFix(system.ID, result)
         if 'enthalpy' in self.targetProperties and '.enthalpy' not in result:
             structure = result.getProperty('structure', extension='atomistic')
             pressure = system.getProperty('externalPressure', suffix='origin')
@@ -67,24 +67,43 @@ class AtomisticStage:
         system.addFlavour(self.tag, result)
         self.checkAndFixMolecules(system)
 
-    def systemCheckAndFix(self, system):
+    def systemCheckAndFix(self, ID, system):
         """
         Checks if given system complies with set up constraints.
         If it does, make certain adjustments, like align the system along required axis.
         :param system: system to be checked and fixed
         """
+        goodStructure = True
         structure = system.getProperty('structure', extension='atomistic')
-        cell = system.getProperty('cell', extension='atomistic')
-        minDistMatrix = self.target.utilities.bondUtility.getDistances(structure.getAtomTypes(),
-                                                                       self.target.utilities.conditions.externalPressure)
-        goodStructure = self.target.utilities.simpleMoleculeUtility.checkMinDistances(system, minDistMatrix) \
-                        and self.target.utilities.cellUtility.isGoodCell(cell)
-        # and self.compositionSpace.isGoodComposition(self.simpleMoleculeUtility.composition(system))
+        # if goodStructure:
+        #     composition = self.target.utilities.simpleMoleculeUtility.composition(system)
+        #     goodStructureInc = goodStructure and self.target.utilities.compositionSpace.isGoodComposition(composition)
+        #     goodStructure = goodStructure and goodStructureInc
+        #     if not goodStructureInc:
+        #         logger.info(f'system {ID} violates composition constraints')
         if goodStructure:
-            goodStructure = goodStructure and self.target.utilities.bondUtility.isConnected(structure)
+            minDistMatrix = self.target.utilities.bondUtility.getDistances(structure.getAtomTypes(),
+                                                                           self.target.utilities.conditions.externalPressure)
+            goodStructureInc = self.target.utilities.simpleMoleculeUtility.checkMinDistances(system, minDistMatrix)
+            goodStructure = goodStructure and goodStructureInc
+            if not goodStructureInc:
+                logger.info(f'system {ID} violates minimal distances constraints')
+        if goodStructure:
+            cell = system.getProperty('cell', extension='atomistic')
+            goodStructureInc = self.target.utilities.cellUtility.isGoodCell(cell)
+            goodStructure = goodStructure and goodStructureInc
+            if not goodStructureInc:
+                logger.info(f'system {ID} violates cell shape constraints')
+        if goodStructure:
+            goodStructureInc = self.target.utilities.bondUtility.isConnected(structure)
+            goodStructure = goodStructure and goodStructureInc
+            if not goodStructureInc:
+                logger.info(f'system {ID} falls into isolated components')
+        if goodStructure:
             cell = structure.getRectifiedCell()
             coordinates = cell.cartesianToFractional(structure.getCartesianCoordinates())
-            if self.target.utilities.cellUtility.getDim() == 1 or self.target.utilities.cellUtility.getDim() == 2:
+            dim = self.target.utilities.cellUtility.getDim()
+            if dim == 1 or dim == 2:
                 cell = cell.getAlignedCell(self.target.utilities.cellUtility.getAxis())
             structure = type(structure).initFromFractionalCoordinates(structure.getAtomTypes(), coordinates, cell,
                                                                       edges=structure.edges)
