82eb8d7b91c467ebd67b82759e053fa48b05d4a9
Vladimir Baturin <vsbat@yandex.ru>
2024-01-15 18:00:04 +0100

    bug reproduced

--- USPEX/Atomistic/unittests/SimpleMoleculeUtility_Test.py
+++ USPEX/Atomistic/unittests/SimpleMoleculeUtility_Test.py
@@ -3,7 +3,7 @@ import unittest
 from pathlib import Path
 
 from ...Optimizers.PoolEntry import PoolEntry, EntryFlavour
-from ...components import SimpleMoleculeUtility, Atomistic
+from ...components import SimpleMoleculeUtility, Atomistic, AtomicStructureRepresentation, BondUtility
 
 PATH_WITH_TESTS = Path(__file__).parent
 
@@ -12,15 +12,22 @@ class SimpleMoleculeUtility_Test(unittest.TestCase):
 
     def setUp(self) -> None:
         PoolEntry.createEngine(":memory:")
-        self.utility = SimpleMoleculeUtility()
 
     def test_checkMinDistances(self):
         atomistic = Atomistic()
         extensions = {'atomistic': atomistic.propertyExtension(atomistic)}
         badDistFilePath = PATH_WITH_TESTS / "badIntermolDist_POSCAR.uspex"
-        molPath = PATH_WITH_TESTS
+        molPath = PATH_WITH_TESTS / "AlH6.xyz"
         badDistSys = Atomistic.readAtomicStructures(badDistFilePath)
-        molecules = {'mol_alh6': }
+        molecules = {'mol_alh6': AtomicStructureRepresentation.readXYZ(molPath.as_posix())}
+        custom_iondist = {'H H': 1.9}
+        bondUtility = BondUtility(volumeType=0, ionDistances=custom_iondist, cutoff='vdw')
+        simpleMoleculeUtility = SimpleMoleculeUtility(molecules)
         system = PoolEntry.newEntry(
-            EntryFlavour(extensions=extensions, **{'.howCome': 'Seeds', '.parent': None}, **badDistSys))
+            EntryFlavour(extensions=extensions, **{'.howCome': 'Seeds', '.parent': None}, **badDistSys[0]))
+        structure = system.getProperty('structure', extension='atomistic')
+        minDistMatrix = bondUtility.getDistances(structure.getAtomTypes(), 0)
+        self.assertTrue(simpleMoleculeUtility.checkMinDistances(system, minDistMatrix))
+
+
 
