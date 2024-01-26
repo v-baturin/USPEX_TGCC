4dae90ad5a2dc9515fd7492e0a84043048897453
Dmitry Rybkovskiy <rybkovskiyd@gmail.com>
2024-01-26 14:58:50 +0000

    Fix DFTBplus_Interface.py

--- USPEX/Stages/Interfaces/DFTBplus_Interface.py
+++ USPEX/Stages/Interfaces/DFTBplus_Interface.py
@@ -131,7 +131,7 @@ class DFTBplus_Interface:
     def readOutput(self, system, calcFolder: Path):
         with open(calcFolder / 'pbc', 'rt') as f:
             pbc = tuple(int(c) for c in f.read().split())
-        atoms = read_gen(calcFolder / self.geometry_file)
+        atoms = read_gen(calcFolder / self.out_geometry_file)
         atoms.set_pbc(pbc)
         new_structure = self.AtomicStructureRepresentation.fromAtoms(atoms)
 
