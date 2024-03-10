4fa362b76d1f9f0f633ba3afdf9e4062a3a4bd4e
Vladimir Baturin <vsbat@yandex.ru>
2024-03-10 11:00:12 +0100

    remove recursion-provoking check in RadialDistributionFunctions

--- USPEX/Expressions/Functions/RadialDistributionFunctions.py
+++ USPEX/Expressions/Functions/RadialDistributionFunctions.py
@@ -10,8 +10,7 @@ class RadialDistributionFunctions:
 
         :return: calculate or retrieve structure fingerprint of a system.
         """
-        if not 'radialDistributionUtility.structureFingerprint' in system:
-            self.utility.calcFingerprint(system)
+        self.utility.calcFingerprint(system)
         return system['radialDistributionUtility.structureFingerprint']
 
     def complexFingerprint(self, system):
@@ -22,8 +21,7 @@ class RadialDistributionFunctions:
 
         :return: calculate or retrieve structure fingerprint of a system.
         """
-        if not 'radialDistributionUtility.complexFingerprint' in system:
-            self.utility.calcFingerprint(system)
+        self.utility.calcFingerprint(system)
         return system['radialDistributionUtility.complexFingerprint']
 
     def order(self, system):
@@ -35,8 +33,7 @@ class RadialDistributionFunctions:
         :return: calculate or retrieve list of atomic *local orders* of a system.
             *Local order* is a measure of atom surrounding being regular.
         """
-        if not 'radialDistributionUtility.order' in system:
-            self.utility.calcFingerprint(system)
+        self.utility.calcFingerprint(system)
         return system['radialDistributionUtility.order']
 
     def averageOrder(self, system):
@@ -48,8 +45,7 @@ class RadialDistributionFunctions:
         :return: calculate or retrieve average atomic *local order* of a system.
             *Local order* is a measure of atom surrounding being regular.
         """
-        if not 'radialDistributionUtility.averageOrder' in system:
-            self.utility.calcFingerprint(system)
+        self.utility.calcFingerprint(system)
         return system['radialDistributionUtility.averageOrder']
 
     def structureOrder(self, system):
@@ -61,8 +57,7 @@ class RadialDistributionFunctions:
         :return: calculate or retrieve *structure order* of a system.
             *Structure order* is a measure of structure being regular.
         """
-        if not 'radialDistributionUtility.structureOrder' in system:
-            self.utility.calcFingerprint(system)
+        self.utility.calcFingerprint(system)
         return system['radialDistributionUtility.structureOrder']
 
     def quasientropy(self, system):
@@ -74,6 +69,5 @@ class RadialDistributionFunctions:
         :return: calculate quasientropy of structure.
         """
 
-        if not 'radialDistributionUtility.quasientropy' in system:
-            self.utility.calcFingerprint(system)
+        self.utility.calcFingerprint(system)
         return system['radialDistributionUtility.quasientropy']
