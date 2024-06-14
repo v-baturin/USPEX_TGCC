782ee24555b5c4c761feb54271f7b001ad3f7900
Vladimir Baturin <vsbat@yandex.ru>
2024-06-14 12:47:01 +0200

    aggregate squeue requests

--- USPEX/Stages/TaskManagers/TGCC.py
+++ USPEX/Stages/TaskManagers/TGCC.py
@@ -7,6 +7,7 @@ USPEX.Stages.TaskManagers.TGCC
 import logging
 
 from pathlib import Path
+from datetime import datetime, timedelta
 
 logger = logging.getLogger(__name__)
 
@@ -18,13 +19,20 @@ class TGCC:
 
     type = 'TGCC'
     _RUNSCRIPT = 'jobscript'
+    _QUEUE_REFRESH_DELAY_SECONDS = 300
 
-    def __init__(self, header : str, connector):
+    def __init__(self, header : str, connector, refreshDelay : int = None):
         self.header = header
         self.connector = connector
+        self.jobsStatusCache = dict()
+        self.queueRefreshDelay = self._QUEUE_REFRESH_DELAY_SECONDS if refreshDelay is None else refreshDelay
+        self.cacheTime = datetime.now()
 
-    def _prepareSubmission(self, COMMAND_EXEC : str, JOB_NAME : str,
-                                 inputFile : str, outputFile : str, errorFile : str) -> str:
+    def _prepareSubmission(self, COMMAND_EXEC : str,
+                                 JOB_NAME : str,
+                                 inputFile : str,
+                                 outputFile : str,
+                                 errorFile : str) -> str:
         '''
         Preparing jobscript for submission
         :param commandExec:
@@ -82,6 +90,7 @@ class TGCC:
 
         jobID = self._parseJobID(out, err)
         logger.info(f"Job in {calcFolder}. ID = {jobID}")
+        self.jobsStatusCache[jobID] = 'PD'
         return jobID
 
     def _parseJobID(self, output : str, error : str) -> int:
@@ -93,21 +102,42 @@ class TGCC:
 
         return int(output.split()[-1])
 
-    async def isReady(self, jobID : int):
-        returncode, out, err = await self.connector.execute(f'squeue -t all -j {jobID}')
-        if 'invalid job' in err.lower():
-            return True
-        header, job, *_ = out.split('\n')
-        i = header.split().index('ST')
-        status = job.split()[i]
-        return status == 'CD' or status == 'F' or status == 'CA' or status == 'S'
+    async def updateCache(self):
+        # Execute the command to get all jobs status
+        returncode, out, err = await self.connector.execute('squeue -t all -u $USER')
 
-    async def isExist(self, jobID : int):
-        returncode, out, err = await self.connector.execute(f'squeue -j {jobID}')
-        header, job, *_ = out.split('\n')
-        i = header.split().index('ST')
-        status = job.split()[i]
-        return status == 'R' or status == 'PD'
+        # Parse the output and update the cache
+        lines = out.split('\n')
+        header = lines[0].split()
+        status_index = header.index('ST')
+        job_id_index = header.index('JOBID')
+
+        new_cache = dict()
+        if len(lines) > 1:
+            for line in lines[1:]:
+                if line.strip():
+                    parts = line.split()
+                    job_id = int(parts[job_id_index])
+                    status = parts[status_index]
+                    new_cache[job_id] = status
+
+        self.jobsStatusCache = new_cache
+        self.cacheTime = datetime.now()
+
+    async def ensureCacheIsFresh(self):
+        # Check if the cache is older than self.queueRefreshDelay seconds and update if necessary
+        if datetime.now() - self.cacheTime > timedelta(seconds=self.queueRefreshDelay):
+            await self.updateCache()
+
+    async def isReady(self, jobID: int):
+        await self.ensureCacheIsFresh()
+        status = self.jobsStatusCache.get(jobID, False)
+        return status in {'CD', 'F', 'CA', 'S', False}
+
+    async def isExist(self, jobID: int):
+        await self.ensureCacheIsFresh()
+        status = self.jobsStatusCache.get(jobID, False)
+        return status in {'R', 'PD'} if status else status
 
     async def kill(self, jobID : int):
         returncode, out, err = await self.connector.execute(f'ccc_mdel {jobID}')
