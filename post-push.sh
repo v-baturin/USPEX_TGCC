60b3694c6f662f7cf2d66a75de9701d814509823
Vladimir Baturin <vsbat@yandex.ru>
2024-06-14 11:02:25 +0200

    add password prompt

--- post-push.sh
+++ post-push.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
+SERVER2="irene_vb:/ccc/work/cont003/gen6175/baturinv/USPEX/Common_v2"
 
-# Remote servers
+# Extract the server hostname for the prompt
+SERVER_NAME=$(echo "$SERVER2" | cut -d'@' -f2 | cut -d':' -f1)
 
-#SERVER0="crivellc@irene-amd-fr.ccc.cea.fr:/ccc/work/cont003/gen6175/crivellc/USPEX/USPEX_TGCC-TGCC_re"
-#SERVER1="baturinv@irene-fr.ccc.cea.fr:/ccc/work/cont003/gen6175/baturinv/USPEX/Common_DB"
-SERVER2="baturinv@irene-fr.ccc.cea.fr:/ccc/work/cont003/gen6175/baturinv/USPEX/Common_v2"
-
-# Hardcoded passwords
-PASSWORD_VB="Nin@18031938"
-#PASSWORD2="qdkgov38!Gfd"
+# Prompt the user for the password with the server name included
+read -sp "Enter password for $SERVER_NAME: " PASSWORD_VB
+echo
 
 # Function to synchronize the current directory with remote servers using rsync
 synchronize_with_servers() {
   echo "Synchronizing current directory with remote servers..."
-
-  # Synchronize with remote servers using rsync and sshpass
   sshpass -p "$PASSWORD_VB" rsync --progress --recursive --compress --exclude="unittests" --exclude="testFolder" --exclude=".git" --exclude="uspex.egg-info/" --exclude="venv/" --exclude=".idea/" --exclude="post-push.sh" ./ "$SERVER2"
-#  sshpass -p "$PASSWORD2" rsync --progress --recursive --compress --exclude=".git" --exclude="uspex.egg-info/" --exclude="venv/" --exclude=".idea/" --exclude="post-push.sh" ./ "$SERVER2"
-
   echo "Current directory synchronized with remote servers successfully."
 }
-
-# Execute the synchronization function
 synchronize_with_servers
