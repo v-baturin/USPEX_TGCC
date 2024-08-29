f8a549de184e2bb36d5947a8ec148175ce6c0dff
Vladimir Baturin <vsbat@yandex.ru>
2024-08-29 15:27:37 +0200

    set NUMEXPR_MAX_THREADS also to 1

--- USPEX/main.py
+++ USPEX/main.py
@@ -62,6 +62,7 @@ def main():
         shellEnv = os.environ.copy()
         os.environ['MKL_NUM_THREADS'] = '1'
         os.environ['NUMEXPR_NUM_THREADS'] = '1'
+        os.environ['NUMEXPR_MAX_THREADS'] = '1'
         os.environ['OMP_NUM_THREADS'] = '1'
     else:
         shellEnv = None
