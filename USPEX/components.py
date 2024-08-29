45981f4c73f7dc055a1109fa62f04aedc28fe406
Vladimir Baturin <vsbat@yandex.ru>
2024-08-29 19:46:05 +0200

    remove MOF_random

--- USPEX/components.py
+++ USPEX/components.py
@@ -39,9 +39,9 @@ from .Atomistic.Operators.RemoveAtom import RemoveAtom
 from .Atomistic.Operators.TeleportAtom import TeleportAtom
 from .Atomistic.Operators.Seeds import Seeds
 from .Atomistic.Operators.CoreAdsorbantRandomGenerator import CoreAdsorbantRandomGenerator
-from .Atomistic.MofUtility import MofUtility
-from .Atomistic.Operators.MOF_Heredity import MOF_Heredity
-from .Atomistic.Operators.MOF_Random import MOF_Random
+#from .Atomistic.MofUtility import MofUtility
+#from .Atomistic.Operators.MOF_Heredity import MOF_Heredity
+#from .Atomistic.Operators.MOF_Random import MOF_Random
 # --------------------------------------------- Targets--- ----------------------------------------------------------
 from .Generators.Target import Target
 Target.registerTarget('Atomistic',
@@ -51,7 +51,7 @@ Target.registerTarget('Atomistic',
                                  JunctionUtility, SymbolsFactoryUtility],
                       hybridizations=[Heredity],
                       mutations=[Softmodemutation, Permutation, Transmutation, AddAtom, RemoveAtom, TeleportAtom],
-                      creations=[RandTop, RandSym, RandSymPyXtal, CoreAdsorbantRandomGenerator, MOF_Random],
+                      creations=[RandTop, RandSym, RandSymPyXtal, CoreAdsorbantRandomGenerator],  # MOF_Random],
                       seeds=Seeds,
                       defaultMetric='radialDistributionUtility')
 # --------------------------------------------- Interfaces ----------------------------------------------------------
--- post-push.sh
+++ post-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SERVER2="irene_vb:/ccc/work/cont003/gen6175/baturinv/USPEX/Common_onthefly"
+SERVER2="irene_vb:/ccc/work/cont003/gen6175/baturinv/USPEX/Common"
 
 # Extract the server hostname for the prompt
 SERVER_NAME=$(echo "$SERVER2" | cut -d'@' -f2 | cut -d':' -f1)
