453b059ca3cb2088c5c9e62bdcf667ccd4acb5c0
Vladimir Baturin <vsbat@yandex.ru>
2025-05-07 21:09:54 +0900

    remove redundant message from post-push

--- post-push.sh
+++ post-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SERVER2="irene_vb:/ccc/work/cont003/gen6175/baturinv/USPEX/Common"
+SERVER2="irene_vb_hydra:/ccc/work/cont003/gen6175/baturinv/USPEX/Common"
 
 # Extract the server hostname for the prompt
 SERVER_NAME=$(echo "$SERVER2" | cut -d'@' -f2 | cut -d':' -f1)
@@ -12,6 +12,6 @@ echo
 synchronize_with_servers() {
   echo "Synchronizing current directory with remote servers..."
   sshpass -p "$PASSWORD_VB" rsync --progress --recursive --compress --exclude="unittests" --exclude="testFolder" --exclude=".git" --exclude="uspex.egg-info/" --exclude="venv/" --exclude=".idea/" --exclude="post-push.sh" ./ "$SERVER2"
-  echo "Current directory synchronized with remote servers successfully."
+#  echo "Current directory synchronized with remote servers successfully."
 }
 synchronize_with_servers
