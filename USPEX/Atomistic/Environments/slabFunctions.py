1804b0901dcd187c4a2709d364d81a2090765975
Vladimir Baturin <vsbat@yandex.ru>
2025-05-22 11:51:43 +0900

    update pymatgen compatibility

--- USPEX/Atomistic/Environments/slabFunctions.py
+++ USPEX/Atomistic/Environments/slabFunctions.py
@@ -4,7 +4,7 @@ import logging
 from pymatgen.analysis.interfaces.zsl import ZSLGenerator
 from pymatgen.analysis.interfaces.coherent_interfaces import get_2d_transform, Deformation
 
-from pymatgen.analysis.gb.grain import GrainBoundaryGenerator
+from pymatgen.core.interface import GrainBoundaryGenerator
 from pymatgen.core.surface import SlabGenerator
 from pymatgen.core import Structure
 from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
