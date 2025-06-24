44745f62f7a46dfa8eb2a5f0fc7b00e627654db4
Vladimir Baturin <vsbat@yandex.ru>
2025-06-24 17:58:08 +0900

    add tgcc_flux that is just shell with some flux errors signaling

---
+++ USPEX/Stages/TaskManagers/TGCC_flux.py
@@ -0,0 +1,60 @@
+"""
+USPEX.Stages.TaskManagers.SHELL
+===============================
+
+.. codeauthor:: Pavel Bushlanov <paulbush@mail.ru>
+
+"""
+import os
+import logging
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+class SHELL:
+
+    shortname = 'SHELL'
+
+    def __init__(self, connector):
+        """
+        :param connector: for remote submission
+        :return:
+        """
+
+        self.connector = connector
+
+    async def submit(self, command: str,
+                           jobname: str,
+                           input: str,
+                           output: str,
+                           error: str,
+                           calcFolder: Path) -> int:
+
+        # if not os
+        uri = os.environ.get('FLUX_URI')
+        if uri and uri.startswith('local://'):
+            sock = uri[len('local://'):]
+            if not os.path.exists(sock):
+                logger.error(f'Flux-broker: socket {sock} is lost. Broker down!')
+        with open(calcFolder/input, 'r') as fi,\
+                open(calcFolder/output, 'w') as fo,\
+                open(calcFolder/error, 'w') as fe:
+            returncode, out, err= await self.connector.execute(command, stdin=fi, stdout=fo, stderr=fe, cwd=str(calcFolder))
+            logger.debug('process returned code {}'.format(returncode))
+            if returncode != 0:
+                logger.error(f'Returncode {returncode} != 0, Error={err}, Output={out}')
+            rc, _, _ = await self.connector.execute('flux ping')
+            if rc != 0:
+                logger.error(f'Flux ping returned {rc}: broker down')
+
+        return 0 if returncode == 0 else -1
+
+    async def isReady(self, jobID : int):
+        return True
+
+    async def isExist(self, jobID : int):
+        return False
+
+    async def kill(self, jobID : int):
+        pass
