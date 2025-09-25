2b0d7ca78a4bb48ba09dd9ad10490c1fab3e61da
Vladimir Baturin <vladimir.s.baturin@gmail.com>
2025-09-25 19:41:06 +0900

    fix spelling

--- USPEX/Atomistic/RadialDistributionUtility.py
+++ USPEX/Atomistic/RadialDistributionUtility.py
@@ -342,7 +342,7 @@ class RadialDistributionUtility(object):
         fing -= normalizer.reshape((1, -1, 1))
 
         atomFings = []
-        weights = {s.short_name: w for s, w in zip(uniqueSimbols, numIons / np.sum(numIons))}
+        weights = {s.short_name: w for s, w in zip(uniqueSymbols, numIons / np.sum(numIons))}
         for s in self.symbols:
             if s not in weights:
                 weights[s] = 0
@@ -352,7 +352,7 @@ class RadialDistributionUtility(object):
             if np.allclose(atom_fing[i], 0):
                 order.append(0.0)
                 continue
-            value = {s.short_name: atom_fing[i, j] for j, s in enumerate(uniqueSimbols)}
+            value = {s.short_name: atom_fing[i, j] for j, s in enumerate(uniqueSymbols)}
             for s in self.symbols:
                 if s not in value:
                     value[s] = np.zeros(N_Bins, dtype=float)
@@ -365,7 +365,7 @@ class RadialDistributionUtility(object):
         molOrder = np.fromiter((order[np.asarray(inds)].sum()/len(inds) for inds in disassembler.indices), dtype=float)
         a_order = np.mean(order[np.isfinite(order)]) if np.any(np.isfinite(order)) else np.nan
 
-        fing = {(s1.short_name, s2.short_name): fing[i, j] for i, s1 in enumerate(uniqueSimbols) for j, s2 in enumerate(uniqueSimbols)}
+        fing = {(s1.short_name, s2.short_name): fing[i, j] for i, s1 in enumerate(uniqueSymbols) for j, s2 in enumerate(uniqueSymbols)}
         fingerprint = Fingerprint(value=fing, weights=self._fingerprintWeights(structure),
                                   delta=self.delta)
         s_order = fingerprint.order
@@ -378,7 +378,7 @@ class RadialDistributionUtility(object):
         for i in range(numIons.shape[0]):
             if numIons[i] > 1:
                 tmp = 0
-                indices = np.flatnonzero(np.equal(newAtomTypes, uniqueSimbols[i]))
+                indices = np.flatnonzero(np.equal(newAtomTypes, uniqueSymbols[i]))
                 comb = list(combinations(indices, 2))
                 for j1, j2 in comb:
                     tmp_fing1 = atomFings[j1]
